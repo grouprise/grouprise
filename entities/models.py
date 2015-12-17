c2330421c1f48cbdc27fd28d338e1ce6e745bc94
Robert <aldrin@systemausfall.org>
2015-12-17 11:44:58 +0100

    added additional group fields

--- entities/models.py
+++ entities/models.py
@@ -2,8 +2,12 @@ from django.db import models
 
 
 class Group(models.Model):
+    address = models.TextField(blank=True)
+    date_created = models.DateField(auto_now_add=True)
+    date_founded = models.DateField(null=True, blank=True)
     name = models.CharField(max_length=255)
     slug = models.SlugField()
+    url = models.URLField(blank=True)
 
     def __str__(self):
         return self.name
--- stadt/settings.py
+++ stadt/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap3',
     'entities',
 ]
 
@@ -56,7 +57,9 @@ ROOT_URLCONF = 'stadt.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'stadt', 'templates'),
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
