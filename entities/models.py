c4bc5f51c8d5fadabeceae4aa4e5711f980d4a83
Robert <aldrin@systemausfall.org>
2015-12-26 16:43:25 +0100

    renamed logo to avatar

--- entities/models.py
+++ entities/models.py
@@ -11,9 +11,9 @@ class Gestalt(models.Model):
 
 class Group(models.Model):
     address = models.TextField(blank=True)
+    avatar = models.ImageField(blank=True)
     date_created = models.DateField(auto_now_add=True)
     date_founded = models.DateField(null=True, blank=True)
-    logo = models.ImageField(blank=True)
     name = models.CharField(max_length=255)
     slug = models.SlugField()
     url = models.URLField(blank=True)
