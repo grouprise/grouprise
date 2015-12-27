191e9c901aff5f07e4a5b2716990fa8ef67775b6
Robert <aldrin@systemausfall.org>
2015-12-27 10:54:07 +0100

    created basic content models

--- content/models.py
+++ content/models.py
@@ -1,5 +1,26 @@
 from django.db import models
 
 
-class Content(models.Model):
+class Base(models.Model):
+    author = models.ForeignKey('entities.Gestalt')
+    date_created = models.DateField(auto_now_add=True)
+    text = models.TextField(blank=True)
+
+    class Meta:
+        abstract = True
+
+
+class Comment(Base):
+    content = models.ForeignKey('Content')
+
+
+class Content(Base):
+    title = models.CharField(max_length=255)
+    views = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.title
+
+
+class Article(Content):
     pass
--- entities/models.py
+++ entities/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Gestalt(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='gestalt')
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
     
     def __str__(self):
         return self.user.username
@@ -20,6 +20,12 @@ class Group(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class GroupContent(models.Model):
+    content = models.OneToOneField('content.Content')
+    group = models.ForeignKey('Group')
+    pinned = models.BooleanField()
 
 
 class Membership(models.Model):
