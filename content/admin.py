d990efd44db80bfd3b150a0ff702d063872b285a
Robert <aldrin@systemausfall.org>
2015-12-28 09:53:20 +0100

    adding rest of sample content led to small adjustments

--- content/admin.py
+++ content/admin.py
@@ -4,4 +4,7 @@ from . import models
 
 
 admin.site.register(models.Article)
+admin.site.register(models.Comment)
+admin.site.register(models.Event)
+admin.site.register(models.Gallery)
 admin.site.register(models.Image)
--- stadt/settings.py
+++ stadt/settings.py
@@ -111,9 +111,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'de-de'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
