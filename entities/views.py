6e4720b2b4147bace08120e8e4d3e7f020bf914c
Robert <aldrin@systemausfall.org>
2015-12-29 11:51:35 +0100

    added basic gestalt page

--- entities/views.py
+++ entities/views.py
@@ -3,5 +3,9 @@ from django.views import generic
 from . import models
 
 
+class GestaltDetailView(generic.DetailView):
+    model = models.Gestalt
+
+
 class GroupDetailView(generic.DetailView):
     model = models.Group
--- stadt/urls.py
+++ stadt/urls.py
@@ -8,6 +8,7 @@ from entities import views as entities_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^gestalt/(?P<pk>[\w-]+)/$', entities_views.GestaltDetailView.as_view(), name='gestalt-detail'),
     url(r'^(?P<slug>[\w-]+)$', entities_views.GroupDetailView.as_view(), name='group-detail'),
     url(r'^(?P<group_slug>[\w-]+)/(?P<slug>[\w-]+)/$', content_views.ContentDetailView.as_view(), name='content-detail'),
 ] + static.static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
