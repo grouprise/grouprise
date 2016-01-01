cb416176311e4b144288693840e0963d0d898702
Robert <aldrin@systemausfall.org>
2016-01-01 13:58:26 +0100

    added user menu links

--- stadt/urls.py
+++ stadt/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings, urls
 from django.conf.urls import static, url
 from django.contrib import admin
+from django.views import generic
 
 from content import views as content_views
 from entities import views as entities_views
@@ -12,6 +13,7 @@ urlpatterns = [
     # root namespace
     url(r'^admin/', admin.site.urls),
     url(r'^auth/', urls.include('allauth.urls')),
+    url(r'^auth/settings/$', generic.TemplateView.as_view(template_name='entities/gestalt_settings.html'), name='gestalt-settings'),
     url(r'^gestalt/(?P<pk>[\w-]+)/$', entities_views.GestaltDetailView.as_view(), name='gestalt-detail'),
     # groups
     url(r'^(?P<slug>[\w-]+)$', entities_views.GroupDetailView.as_view(), name='group-detail'),
