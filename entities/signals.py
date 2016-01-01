2b2d5f8a96d1ddd2dbb47b9418e83a75d504a848
Robert <aldrin@systemausfall.org>
2016-01-01 15:27:52 +0100

    added default data to group

--- entities/signals.py
+++ entities/signals.py
@@ -1,4 +1,6 @@
 from django.contrib import auth
+from django.contrib.sites import models as sites_models
+from django.core import urlresolvers
 from django.db.models import signals
 from django.dispatch import receiver
 
@@ -8,3 +10,17 @@ from . import models
 @receiver(signals.post_save, sender=auth.get_user_model())
 def user_post_save(sender, instance, **kwargs):
     models.Gestalt.objects.get_or_create(user=instance)
+
+
+@receiver(signals.pre_save, sender=models.Group)
+def group_pre_save(sender, instance, **kwargs):
+    group = instance
+    if not group.address:
+        group.address = group.name
+    if not group.url:
+        group.url = 'http://%(domain)s%(path)s' % {
+                'domain': sites_models.Site.objects.get_current().domain,
+                'path': urlresolvers.reverse('group-detail', args=[group.slug]),
+                }
+    if not group.date_founded:
+        group.date_founded = group.date_created
