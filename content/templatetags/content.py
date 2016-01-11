dfabb736cb4c6d03540a95eeb0ea9fcd6fe38064
Robert <aldrin@systemausfall.org>
2016-01-11 15:21:40 +0100

    added markdown support for content

---
+++ content/templatetags/content.py
@@ -0,0 +1,15 @@
+import markdown as python_markdown
+from django import template
+from django.utils import html, safestring
+
+
+register = template.Library()
+
+
+@register.filter(needs_autoescape=True)
+def markdown(text, autoescape=True):
+    if autoescape:
+        esc = html.conditional_escape
+    else:
+        esc = lambda x: x
+    return safestring.mark_safe(python_markdown.markdown(esc(text)))
