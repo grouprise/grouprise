2b681546269c04e5d2fff38b52d588157f090e4e
Robert <aldrin@systemausfall.org>
2016-01-22 11:15:20 +0100

    added head gallery

--- entities/querysets.py
+++ entities/querysets.py
@@ -2,8 +2,19 @@ from django.db import models
 
 
 class GroupContentQuerySet(models.QuerySet):
+    def galleries(self):
+        return self.exclude(content__gallery=None)
+
     def pinned(self):
         return self.filter(pinned=True)
+
+    def pinned_without_head_gallery(self):
+        pinned = self.pinned()
+        gallery = pinned.galleries().first()
+        if gallery:
+            return pinned.exclude(pk=gallery.pk)
+        else:
+            return pinned
 
     def unpinned(self):
         return self.filter(pinned=False)
