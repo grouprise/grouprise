e150d8c9c09e4ffd468d5dec9d8f6a82b56d0e98
Robert <aldrin@systemausfall.org>
2016-01-22 10:14:30 +0100

    added user fields

--- entities/forms.py
+++ entities/forms.py
@@ -1,14 +1,27 @@
-from crispy_forms import helper
+from crispy_forms import bootstrap, helper, layout
 from django import forms as django_forms
 from django.contrib.auth import models as auth_models
+from django.contrib.sites import shortcuts
 
 
 class User(django_forms.ModelForm):
     class Meta:
         model = auth_models.User
-        fields = ['username']
+        fields = ['first_name', 'last_name', 'username']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        self.fields['username'].label = 'Adresse der Benutzerseite / Pseudonym'
+
         self.helper = helper.FormHelper()
         self.helper.form_tag = False
+        self.helper.layout = layout.Layout(*self.get_layout())
+    
+    def get_layout(self):
+        DOMAIN = shortcuts.get_current_site(None).domain
+        return [
+                bootstrap.PrependedText('username', '%(domain)s/gestalt/' % {'domain': DOMAIN }),
+                'first_name',
+                'last_name',
+                ]
