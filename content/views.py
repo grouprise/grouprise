1008838717081f5df9e211c99ddfa3c973200653
Robert <aldrin@systemausfall.org>
2016-02-11 11:38:27 +0100

    added basic content creation view

--- content/views.py
+++ content/views.py
@@ -8,22 +8,6 @@ from util import views as util_views
 
 
 class ContentNavigationMixin(util_views.GroupMixin, util_views.NavigationMixin):
-    def get_success_url(self):
-        try:
-            return urlresolvers.reverse('group-content', args=[self.get_group().slug, self.object.slug])
-        except entities_models.Group.DoesNotExist:
-            return super().get_success_url()
-
-
-class ContentCreate(rules_views.PermissionRequiredMixin, ContentNavigationMixin, generic.CreateView):
-    model = models.Article
-    permission_required = 'content.create_group_content'
-
-
-class ContentDetail(rules_views.PermissionRequiredMixin, util_views.NavigationMixin, util_views.GroupMixin, generic.DetailView):
-    model = models.Content
-    permission_required = 'content.view_content'
-
     def get_back_url(self):
         try:
             entity = self.get_group()
@@ -32,11 +16,26 @@ class ContentDetail(rules_views.PermissionRequiredMixin, util_views.NavigationMi
         return entity.get_absolute_url()
 
 
+class ContentCreate(rules_views.PermissionRequiredMixin, ContentNavigationMixin, generic.CreateView):
+    fields = ['text', 'title']
+    model = models.Article
+    permission_required = 'content.create_group_content'
+    template_name = 'content/content_form.html'
+
+    def get_permission_object(self):
+        return self.get_group()
+
+
+class ContentDetail(rules_views.PermissionRequiredMixin, ContentNavigationMixin, generic.DetailView):
+    model = models.Content
+    permission_required = 'content.view_content'
+
+
 class ContentList(generic.ListView):
     model = models.Content
 
 
-class ContentUpdate(rules_views.PermissionRequiredMixin, ContentNavigationMixin, util_views.LayoutMixin, generic.UpdateView):
+class ContentUpdate(rules_views.PermissionRequiredMixin, util_views.NavigationMixin, util_views.GroupMixin, util_views.LayoutMixin, generic.UpdateView):
     fields = ['text', 'title']
     layout = [
             'title',
@@ -45,3 +44,9 @@ class ContentUpdate(rules_views.PermissionRequiredMixin, ContentNavigationMixin,
             ]
     model = models.Content
     permission_required = 'content.change_content'
+    
+    def get_success_url(self):
+        try:
+            return urlresolvers.reverse('group-content', args=[self.get_group().slug, self.object.slug])
+        except entities_models.Group.DoesNotExist:
+            return super().get_success_url()
