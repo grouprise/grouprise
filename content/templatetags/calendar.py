36631ef7029cd3e917f584ade5e1d6f0ceb1f6c1
Robert <gestalt@systemausfall.org>
2016-02-24 16:11:57 +0100

    replaced python calendar by template based one

--- content/templatetags/calendar.py
+++ content/templatetags/calendar.py
@@ -1,13 +1,42 @@
 import calendar as python_calendar
 import datetime
 from django import template
-from django.utils import safestring
-
 
 register = template.Library()
 
+_today = datetime.date.today()
 
-@register.simple_tag
+
+class Calendar(python_calendar.LocaleHTMLCalendar):
+    def formatday(self, day, weekday):
+        if day == 0:
+            return '' # day outside month
+        else:
+            return day
+
+    def formatmonthname(self, theyear=_today.year, themonth=_today.month):
+        with python_calendar.different_locale(self.locale):
+            return '%s %s' % (python_calendar.month_name[themonth], theyear)
+
+    def formatmonthweeks(self, theyear=_today.year, themonth=_today.month):
+        return [self.formatweek(week) for week in self.monthdays2calendar(theyear, themonth)]
+
+    def formatweek(self, theweek):
+        return [self.formatday(d, wd) for (d, wd) in theweek]
+
+    def formatweekday(self, day):
+        with python_calendar.different_locale(self.locale):
+            return python_calendar.day_abbr[day]
+
+    def formatweekheader(self):
+        return [self.formatweekday(i) for i in self.iterweekdays()]
+
+
+@register.inclusion_tag('calendar/_calendar.html')
 def calendar():
-    today = datetime.date.today()
-    return safestring.mark_safe(python_calendar.LocaleHTMLCalendar().formatmonth(today.year, today.month))
+    c = Calendar()
+    return {
+            'days': c.formatweekheader(), 
+            'month': c.formatmonthname(), 
+            'weeks': c.formatmonthweeks(),
+            }
