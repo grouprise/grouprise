d81603361100e2b450f027ff8b92c161762c56ff
Robert <gestalt@systemausfall.org>
2016-03-14 08:39:48 +0100

    fixed url patterns+

--- entities/urls.py
+++ entities/urls.py
@@ -2,9 +2,9 @@ from . import views
 from django.conf import urls
 
 urlpatterns = [
-    urls.url(r'^stadt/gestalt/(?P<pk>[0-9]+)/edit/$', views.GestaltUpdate.as_view(), name='gestalt-update'),
-    urls.url(r'^stadt/group/membership/add/group=(?P<group_slug>[\w-]+)$', views.GroupMembershipCreate.as_view(), name='group-membership-create'),
-    urls.url(r'^stadt/group/membership/(?P<pk>[0-9]+)/delete/$', views.GroupMembershipDelete.as_view(), name='group-membership-delete'),
-    urls.url(r'^stadt/group/new/$', views.GroupCreate.as_view(), name='group-create'),
-    urls.url(r'^stadt/group/(?P<pk>[0-9]+)/edit/$', views.GroupUpdate.as_view(), name='group-update'),
+    urls.url(r'^gestalt/(?P<pk>[0-9]+)/edit/$', views.GestaltUpdate.as_view(), name='gestalt-update'),
+    urls.url(r'^group/membership/add/group=(?P<group_slug>[\w-]+)$', views.GroupMembershipCreate.as_view(), name='group-membership-create'),
+    urls.url(r'^group/membership/(?P<pk>[0-9]+)/delete/$', views.GroupMembershipDelete.as_view(), name='group-membership-delete'),
+    urls.url(r'^group/new/$', views.GroupCreate.as_view(), name='group-create'),
+    urls.url(r'^group/(?P<pk>[0-9]+)/edit/$', views.GroupUpdate.as_view(), name='group-update'),
 ]
