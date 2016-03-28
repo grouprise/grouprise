1594d7ce241e0013577b4804c8a0c40d383e36e6
Robert <gestalt@systemausfall.org>
2016-03-28 15:00:25 +0200

    added authored content to permitted content

--- content/querysets.py
+++ content/querysets.py
@@ -5,8 +5,9 @@ from entities import models as entities_models
 
 class ContentQuerySet(models.QuerySet):
     def permitted(self, user):
+        gestalt = user.gestalt if user.is_authenticated() else None
         user_groups = user.gestalt.group_set.all() if user.is_authenticated() else entities_models.Group.objects.none()
-        return self.filter(models.Q(public=True) | models.Q(groupcontent__group__in=user_groups))
+        return self.filter(models.Q(public=True) | models.Q(groupcontent__group__in=user_groups) | models.Q(author=gestalt))
 
 class EventQuerySet(models.QuerySet):
     def around(self, time):
