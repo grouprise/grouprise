a583f35ae423dc016ddd666aa13ba302fa92a376
Robert <gestalt@systemausfall.org>
2016-04-25 13:16:17 +0200

    added search index template

--- entities/search_indexes.py
+++ entities/search_indexes.py
@@ -3,7 +3,7 @@ from haystack import indexes
 
 
 class Group(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True, model_attr='name')
+    text = indexes.CharField(document=True, use_template=True)
 
     def get_model(self):
         return models.Group
