620516ae74ddd3f998fe78717147cfdeab9f7585
Robert <gestalt@systemausfall.org>
2016-05-09 15:41:20 +0200

    Fix group event display by new override filter

--- utils/templatetags/utils.py
+++ utils/templatetags/utils.py
@@ -5,3 +5,10 @@ register = template.Library()
 @register.filter
 def limit(indexable, count):
     return indexable[:count]
+
+@register.filter
+def override(override, overridden):
+    if type(override) == type(overridden):
+        return override
+    else:
+        return overridden
