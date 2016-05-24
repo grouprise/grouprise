4eb00bfe5ddbada254fb53728c4c682a7380a30d
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2016-05-24 20:00:51 +0200

    basic translation for date selector

--- res/js/components/date.js
+++ res/js/components/date.js
@@ -1,3 +1,16 @@
 import Pikaday from "pikaday";
 
-export default (el) => new Pikaday({ field: el });
+
+const translation_de = {
+    previousMonth : 'Vorheriger Monat',
+    nextMonth     : 'Nächster Monat',
+    months        : ['Januar','Februar','März','April','Mai','Juni','Juli','August','September','Oktober','November','Dezember'],
+    weekdays      : ['Sonntag','Montag','Dienstag','Mittwoch','Donnerstag','Freitag','Samstag'],
+    weekdaysShort : ['So','Mo','Di','Mi','Do','Fr','Sa']
+};
+
+export default (el) => new Pikaday({
+    field: el,
+    firstDay: 1,
+    i18n: translation_de
+});
