20d430928b271bec5363bc5c1510a1c05213d467
Robert <gestalt@systemausfall.org>
2016-05-26 15:27:58 +0200

    Configure editor

--- res/js/components/editor.js
+++ res/js/components/editor.js
@@ -7,22 +7,84 @@ export default (el) => new SimpleMDE({
         allowAtxHeaderWithoutSpace: true,
         strikethrough: false,
     },
-    renderingConfig: {
-        singleLineBreaks: false,
-    },
     shortcuts: {
-        "toggleBold": "Shift-Cmd-F",
+        "toggleBold": "Shift-Ctrl-F",
+        "toggleItalic": "Ctrl-I",
+        "undo": "Ctrl-Z",
+        "redo": "Ctrl-Y",
+        "toggleUnorderedList": "Shift-F12",
+        "toggleOrderedList": "F12",
+        "toggleHeading2": "Ctrl-2",
+        "toggleHeading3": "Ctrl-3",
     },
     spellChecker: false,
     status: false,
-    /*
     toolbar: [
+        {
+            name: "undo",
+            action: SimpleMDE.undo,
+            className: "fa fa-undo no-disable",
+            title: "Rückgängig"
+        },
+        {
+            name: "redo",
+            action: SimpleMDE.redo,
+            className: "fa fa-repeat no-disable",
+            title: "Wiederholen"
+        },
+        "|",
+        {
+            name: "italic",
+            action: SimpleMDE.toggleItalic,
+            className: "fa fa-italic",
+            title: "Kursiv",
+        },
         {
             name: "bold",
             action: SimpleMDE.toggleBold,
             className: "fa fa-bold",
             title: "Fett",
         },
+        "|",
+        {
+            name: "unordered-list",
+            action: SimpleMDE.toggleUnorderedList,
+            className: "fa fa-list-ul",
+            title: "Einfache Liste",
+        },
+        {
+            name: "ordered-list",
+            action: SimpleMDE.toggleOrderedList,
+            className: "fa fa-list-ol",
+            title: "Nummerierte Liste",
+        },
+        "|",
+        {
+            name: "heading-2",
+            action: SimpleMDE.toggleHeading2,
+            className: "fa fa-header",
+            title: "Überschrift"
+        },
+        {
+            name: "heading-3",
+            action: SimpleMDE.toggleHeading3,
+            className: "fa fa-header fa-header-x fa-header-2",
+            title: "Unterüberschrift"
+        },
+        "|",
+        {
+            name: "link",
+            action: SimpleMDE.drawLink,
+            className: "fa fa-link",
+            title: "Link/Verweis",
+        },
+        {
+            name: "image",
+            action: SimpleMDE.drawImage,
+            className: "fa fa-picture-o",
+            title: "Bild",
+        },
+        "|",
         {
             name: "guide",
             action: "https://simplemde.com/markdown-guide",
@@ -30,5 +92,4 @@ export default (el) => new SimpleMDE({
             title: "Hilfe",
         },
     ],
-    */
 });
