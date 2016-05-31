2a5eedc863edfcd9b699390c869bd47c81061e9c
Robert <gestalt@systemausfall.org>
2016-05-31 11:46:13 +0200

    Add confirm action

--- account/views.py
+++ account/views.py
@@ -4,6 +4,19 @@ from crispy_forms import layout
 from django.views.generic import edit as edit_views
 from utils import views as util_views
 
+class Confirm(util_views.ActionMixin, edit_views.FormMixin, views.ConfirmEmailView):
+    action = 'E-Mail-Adresse bestätigen'
+    ignore_base_templates = True
+    layout = layout.HTML('<p>Ist <a href="mailto:{{ confirmation.email_address.email }}">{{ confirmation.email_address.email }}</a> eine E-Mail-Adresse des Benutzers <em>{{ confirmation.email_address.user }}</em>?</p>')
+    permission = 'account.confirm'
+
+    def get_context_data(self, **kwargs):
+        # as FormMixin doesn't call get_context_data() on super() we have to call it explicitly
+        return views.ConfirmEmailView.get_context_data(self, **super().get_context_data(**kwargs))
+
+    def get_parent(self):
+        return self.get_redirect_url()
+
 class Login(util_views.ActionMixin, views.LoginView):
     action = 'Anmelden'
     form_class = forms.LoginForm
