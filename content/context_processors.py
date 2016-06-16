3a8ad9860c84855bc9aa2b39deb3dcca2dcb64a3
Robert <gestalt@systemausfall.org>
2016-06-16 14:10:36 +0200

    Generally show all upcoming events (closes #78)

--- content/context_processors.py
+++ content/context_processors.py
@@ -3,7 +3,7 @@ from . import models
 def events(request):
     return {
             'all_calendar_events': models.Event.objects.permitted(request.user).around(),
-            'all_upcoming_events': models.Event.objects.permitted(request.user).upcoming(),
+            'all_upcoming_events': models.Event.objects.permitted(request.user).upcoming(3),
             }
 
 def statistics(request):
