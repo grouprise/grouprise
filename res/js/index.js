a9adc45c69e213163c904771856426253c1c19b8
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2016-06-16 14:55:07 +0200

    initialize dropzone componetn

--- res/js/index.js
+++ res/js/index.js
@@ -1,11 +1,25 @@
-import { component } from "./util/dom";
+import { component, replace } from "./util/dom";
+import { evented_function } from "./util/events";
 import date from "./components/date";
 import editor from "./components/editor";
 import time from "./components/time";
 import user_content from "./components/user-content";
+import image_upload from "./components/image-upload";
 
-// initialize components on load
-component("date", date);
-component("editor", editor);
-component("time", time);
-component("user-content", user_content);
+// create evented functions
+const _replace = evented_function(replace);
+
+// add evented function event listeners
+_replace.on("call", init);
+
+
+function init(search_in = document) {
+    // initialize components on load
+    component("date", date, search_in);
+    component("editor", editor, search_in);
+    component("time", time, search_in);
+    component("user-content", user_content, search_in);
+    component("image-upload", image_upload, { root: search_in, conf: { replace: _replace } });
+}
+
+init();
