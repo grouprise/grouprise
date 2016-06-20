99620817e4d2e6dbe534ac751a4053f55fec24fa
Robert <gestalt@systemausfall.org>
2016-06-20 11:30:13 +0200

    Hide author information for public group articles (closes #38)

--- content/rules.py
+++ content/rules.py
@@ -2,21 +2,37 @@ from . import models
 import rules
 
 @rules.predicate
-def content_is_permitted(user, content):
+def is_permitted(user, content):
     return content in models.Content.objects.permitted(user)
 
 @rules.predicate
-def is_content_author(user, content):
+def is_author(user, content):
     return content.author == user.gestalt
+
+@rules.predicate
+def is_group_content(user, content):
+    return content.groups.exists()
+
+@rules.predicate
+def is_group_member(user, content):
+    for group in content.groups.all():
+        if user.gestalt in group.members.all():
+            return True
+    return False
+
+@rules.predicate
+def is_public(user, content):
+    return content.public
 
 rules.add_perm('content.view_content_list', rules.always_allow)
 rules.add_perm('content.view_event_day', rules.always_allow)
 rules.add_perm('content.view_help', rules.always_allow)
 
-rules.add_perm('content.view_content', content_is_permitted)
+rules.add_perm('content.view_content', is_permitted)
+rules.add_perm('content.view_author', is_permitted & (~is_group_content | ~is_public | (rules.is_authenticated & (is_author | is_group_member))))
 rules.add_perm('content.create_content', rules.is_authenticated)
-rules.add_perm('content.change_content', rules.is_authenticated & is_content_author)
+rules.add_perm('content.change_content', rules.is_authenticated & is_author)
 
-rules.add_perm('content.create_comment', rules.is_authenticated & content_is_permitted)
-rules.add_perm('content.create_image', rules.is_authenticated & is_content_author)
-rules.add_perm('content.view_image_list', content_is_permitted)
+rules.add_perm('content.create_comment', rules.is_authenticated & is_permitted)
+rules.add_perm('content.create_image', rules.is_authenticated & is_author)
+rules.add_perm('content.view_image_list', is_permitted)
