cd26d51ebec6651a59ec27aef3b3e0826a7d3ba1
Robert <gestalt@systemausfall.org>
2016-06-21 14:42:55 +0200

    Remove trailing slash from url display

--- utils/templatetags/utils.py
+++ utils/templatetags/utils.py
@@ -3,6 +3,12 @@ from django import template
 register = template.Library()
 
 @register.filter
+def cuttrailing(s1, s2):
+    if s1.endswith(s2):
+        return s1[:-len(s2)]
+    return s1
+
+@register.filter
 def limit(indexable, count):
     return indexable[:count]
 
