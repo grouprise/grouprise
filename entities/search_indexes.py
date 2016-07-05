c2190f98bd3a52c815a268c27e2f2184ca8a6214
Robert <gestalt@systemausfall.org>
2016-07-05 10:29:16 +0200

    Remove search index

--- entities/search_indexes.py
+++
@@ -1,9 +0,0 @@
-from . import models
-from haystack import indexes
-
-
-class Group(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True, use_template=True)
-
-    def get_model(self):
-        return models.Group
