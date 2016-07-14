446a2ca45bc5dac73dfe067623e0918eb78f38b9
Robert <gestalt@systemausfall.org>
2016-07-14 15:54:01 +0200

    Removed unused code, flake8 fixes

--- features/subscriptions/notifiers.py
+++ features/subscriptions/notifiers.py
@@ -4,5 +4,7 @@ from entities import models as entities_models
 
 class Subscription:
     def get_recipients_for(instance):
-        subscriptions = models.Subscription.objects.filter(subscribed_to=instance)
-        return entities_models.Gestalt.objects.filter(subscription__in=subscriptions)
+        subscriptions = models.Subscription.objects.filter(
+                subscribed_to=instance)
+        return entities_models.Gestalt.objects.filter(
+                subscription__in=subscriptions)
