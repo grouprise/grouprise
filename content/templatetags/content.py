7e10b454f32f6b53cce8c24ccb42d3fcb0ddea20
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2016-07-20 14:33:58 +0200

    added macro code
    original authors

--- content/templatetags/content.py
+++ content/templatetags/content.py
@@ -6,6 +6,11 @@ from pymdownx import magiclink
 
 register = template.Library()
 
+
+from django import template
+from django.template.base import FilterExpression
+from django.template.loader import get_template
+
 @register.filter(needs_autoescape=True)
 def markdown(text, autoescape=True):
     if autoescape:
@@ -21,3 +26,140 @@ def permitted(content, user):
 @register.filter
 def preview(events):
     return ', '.join(map(lambda e: '{} {}'.format(formats.time_format(timezone.localtime(e.time)), e.title), events))
+
+
+def _setup_macros_dict(parser):
+    # Metadata of each macro are stored in a new attribute
+    # of 'parser' class. That way we can access it later
+    # in the template when processing 'usemacro' tags.
+    try:
+        # Only try to access it to eventually trigger an exception
+        parser._macros
+    except AttributeError:
+        parser._macros = {}
+
+
+class DefineMacroNode(template.Node):
+    def __init__(self, name, nodelist, args):
+
+        self.name = name
+        self.nodelist = nodelist
+        self.args = []
+        self.kwargs = {}
+        for a in args:
+            if "=" not in a:
+                self.args.append(a)
+            else:
+                name, value = a.split("=")
+                self.kwargs[name] = value
+
+    def render(self, context):
+        # empty string - {% macro %} tag does no output
+        return ''
+
+
+@register.tag(name="kwacro")
+def do_macro(parser, token):
+    try:
+        args = token.split_contents()
+        tag_name, macro_name, args = args[0], args[1], args[2:]
+    except IndexError:
+        m = ("'%s' tag requires at least one argument (macro name)"
+             % token.contents.split()[0])
+        raise template.TemplateSyntaxError(m)
+    # TODO: could do some validations here,
+    # for now, "blow your head clean off"
+    nodelist = parser.parse(('endkwacro', ))
+    parser.delete_first_token()
+
+    # Metadata of each macro are stored in a new attribute
+    # of 'parser' class. That way we can access it later
+    # in the template when processing 'usemacro' tags.
+    _setup_macros_dict(parser)
+    parser._macros[macro_name] = DefineMacroNode(macro_name, nodelist, args)
+    return parser._macros[macro_name]
+
+
+class LoadMacrosNode(template.Node):
+    def render(self, context):
+        # empty string - {% loadmacros %} tag does no output
+        return ''
+
+
+@register.tag(name="loadkwacros")
+def do_loadmacros(parser, token):
+    try:
+        tag_name, filename = token.split_contents()
+    except IndexError:
+        m = ("'%s' tag requires at least one argument (macro name)"
+             % token.contents.split()[0])
+        raise template.TemplateSyntaxError(m)
+    if filename[0] in ('"', "'") and filename[-1] == filename[0]:
+        filename = filename[1:-1]
+    t = get_template(filename)
+    macros = t.nodelist.get_nodes_by_type(DefineMacroNode)
+    # Metadata of each macro are stored in a new attribute
+    # of 'parser' class. That way we can access it later
+    # in the template when processing 'usemacro' tags.
+    _setup_macros_dict(parser)
+    for macro in macros:
+        parser._macros[macro.name] = macro
+    return LoadMacrosNode()
+
+
+class UseMacroNode(template.Node):
+
+    def __init__(self, macro, fe_args, fe_kwargs):
+        self.macro = macro
+        self.fe_args = fe_args
+        self.fe_kwargs = fe_kwargs
+
+    def render(self, context):
+
+        for i, arg in enumerate(self.macro.args):
+            try:
+                fe = self.fe_args[i]
+                context[arg] = fe.resolve(context)
+            except IndexError:
+                context[arg] = ""
+
+        for name, default in iter(self.macro.kwargs.items()):
+            if name in self.fe_kwargs:
+                context[name] = self.fe_kwargs[name].resolve(context)
+            else:
+                context[name] = FilterExpression(default,
+                                                 self.macro.parser
+                                                 ).resolve(context)
+
+        return self.macro.nodelist.render(context)
+
+
+@register.tag(name="usekwacro")
+def do_usemacro(parser, token):
+    try:
+        args = token.split_contents()
+        tag_name, macro_name, values = args[0], args[1], args[2:]
+    except IndexError:
+        m = ("'%s' tag requires at least one argument (macro name)"
+             % token.contents.split()[0])
+        raise template.TemplateSyntaxError(m)
+    try:
+        macro = parser._macros[macro_name]
+    except (AttributeError, KeyError):
+        m = "Macro '%s' is not defined" % macro_name
+        raise template.TemplateSyntaxError(m)
+
+    fe_kwargs = {}
+    fe_args = []
+
+    for val in values:
+        if "=" in val:
+            # kwarg
+            name, value = val.split("=")
+            fe_kwargs[name] = FilterExpression(value, parser)
+        else:  # arg
+            # no validation, go for it ...
+            fe_args.append(FilterExpression(val, parser))
+
+    macro.parser = parser
+    return UseMacroNode(macro, fe_args, fe_kwargs)
