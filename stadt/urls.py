76221e6d06b22bba4ad75a4cf0ee1eee15b0b300
Robert <gestalt@systemausfall.org>
2016-08-05 10:59:15 +0200

    Fix trailing slash for group urls #144

--- stadt/urls.py
+++ stadt/urls.py
@@ -2,6 +2,7 @@ from content import views as content_views
 from django.conf import settings, urls
 from django.conf.urls import static
 from django.contrib import admin
+from django.core import urlresolvers
 from django.views import generic
 from entities import views as entities_views
 
@@ -17,6 +18,6 @@ urlpatterns = [
     urls.url(r'^$', content_views.ContentList.as_view(), name='index'),
     urls.url(r'^gestalt/(?P<gestalt_slug>[\w.@+-]+)/$', entities_views.Gestalt.as_view(), name='gestalt'),
     urls.url(r'^gestalt/(?P<gestalt_slug>[\w.@+-]+)/(?P<slug>[\w-]+)/$', content_views.Content.as_view(), name='gestalt-content'),
-    urls.url(r'^(?P<group_slug>[\w-]+)$', entities_views.Group.as_view(), name='group'),
+    urls.url(r'^(?P<group_slug>[\w-]+)/$', entities_views.Group.as_view(), name='group'),
     urls.url(r'^(?P<group_slug>[\w-]+)/(?P<slug>[\w-]+)/$', content_views.Content.as_view(), name='content'),
 ] + static.static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
