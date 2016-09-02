6ddeca9af0c27ef1b96d42da37b4cae4b9acfc97
Robert <gestalt@systemausfall.org>
2016-09-02 16:21:56 +0200

    Fix template name

--- features/memberships/notifications.py
+++ features/memberships/notifications.py
@@ -3,7 +3,6 @@ from core import notifications
 
 class MembershipCreated(notifications.Notification):
     subject = 'Stadtgestalten: In Gruppe aufgenommen'
-    template_name = 'memberships/notifications/membershipcreated.txt'
 
     def get_recipient(self):
         return self.membership.member
