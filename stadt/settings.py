48569b2869b8fd629cee245a9c169ee0878a39f7
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2016-09-08 06:17:39 +0200

    add django debug toolbar

--- stadt/settings.py
+++ stadt/settings.py
@@ -59,17 +59,19 @@ INSTALLED_APPS = [
     'rules.apps.AutodiscoverRulesConfig',
     'sorl.thumbnail',
     'utils',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 ROOT_URLCONF = 'stadt.urls'
@@ -252,3 +254,5 @@ REST_FRAMEWORK = {
         'rest_framework.filters.DjangoFilterBackend',
     ),
 }
+
+INTERNAL_IPS = ("127.0.0.1", )
