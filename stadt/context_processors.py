06d229f3cc9f1c827d2605c8d7ee62d694278389
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2016-09-14 03:00:34 +0200

    fix relative urls in metadata

--- stadt/context_processors.py
+++ stadt/context_processors.py
@@ -5,7 +5,10 @@ from django.conf import settings
 def site(request):
     return {
         'site': shortcuts.get_current_site(request),
-        'site_description': 'Stadtgestalten ist eine neue Internet-Plattform und ermöglicht aktiven, engagierten Menschen und Gruppen in der Stadt, sich unkompliziert zu informieren, zu präsentieren und miteinander zu vernetzen.'
+        'site_description': 'Stadtgestalten ist eine neue Internet-Plattform und ermöglicht aktiven, engagierten '
+                            'Menschen und Gruppen in der Stadt, sich unkompliziert zu informieren, zu präsentieren '
+                            'und miteinander zu vernetzen.',
+        'http_origin': request.build_absolute_uri("/").rstrip("/")
     }
 
 
