dc4302511b4b5b86ec87e5cf12823e609f7990a6
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2016-09-14 18:10:32 +0200

    work-around firefox rendering bug in group header. refs #182

--- res/js/transforms/group-header.js
+++ res/js/transforms/group-header.js
@@ -3,6 +3,6 @@ import { $ } from "../util/dom";
 export default (el) => {
     const height = $(".group-info", el).clientHeight;
     const style = document.createElement("style");
-    style.innerHTML = `.group-gallery .carousel-slides > li img { min-height: ${height}px; }`;
+    style.innerHTML = `.group-gallery .carousel-slides > li img { min-height: ${height + 1}px; }`;
     document.head.appendChild(style);
 };
