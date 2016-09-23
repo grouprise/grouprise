725e6e847ee8655c9151416ebf8338e7b499243e
Robert <gestalt@systemausfall.org>
2016-09-23 11:06:25 +0200

    Mark groups models to be deleted

--- entities/models.py
+++ entities/models.py
@@ -71,11 +71,13 @@ class GestaltContent(models.Model):
     gestalt = models.ForeignKey('Gestalt')
 
 
+# FIXME: can be deleted after sqashing migrations
 def validate_reservation(value):
     if value in ['gestalt', 'stadt']:
         raise exceptions.ValidationError('Die Adresse \'%(value)s\' darf nicht verwendet werden.', params={'value': value}, code='reserved')
 
 
+# FIXME: can be deleted after sqashing migrations
 class AutoSlugField(models.SlugField):
     def __init__(self, *args, **kwargs):
         self.populate_from = kwargs.pop('populate_from')
@@ -98,6 +100,7 @@ class AutoSlugField(models.SlugField):
             return super().pre_save(model_instance, add)
 
 
+# FIXME: can be deleted after sqashing migrations
 class Group(core.models.Model):
     address = models.TextField('Anschrift', blank=True)
     avatar = models.ImageField(blank=True)
@@ -106,49 +109,15 @@ class Group(core.models.Model):
             'Geschlossene Gruppe',
             default=False,
             help_text='Nur Mitglieder können neue Mitglieder aufnehmen.')
-    # content = models.ManyToManyField('content.Content', related_name='old_groups', through='GroupContent')
     date_created = models.DateField(auto_now_add=True)
     date_founded = models.DateField('Gruppe gegründet', null=True, blank=True)
     logo = models.ImageField(blank=True)
     name = models.CharField('Name', max_length=255)
     slug = AutoSlugField('Adresse der Gruppenseite', populate_from='name', reserve=['gestalt', 'stadt'], unique=True)
-    #subscriptions = contenttypes_fields.GenericRelation('subscriptions.Subscription')
     url = models.URLField('Adresse im Web', blank=True)
     description = models.TextField('Kurzbeschreibung', blank=True, default='', max_length=200)
-
-    objects = models.Manager.from_queryset(querysets.Group)()
-
     class Meta:
-        # FIXME: we should use case-insensitive ordering here
-        # https://code.djangoproject.com/ticket/26257
         ordering = ('name',)
-
-    def __str__(self):
-        return self.name
-
-    def get_absolute_url(self):
-        return urlresolvers.reverse('group', args=[type(self).objects.get(pk=self.pk).slug])
-
-    def get_head_gallery(self):
-        return self.content.exclude(gallery=None).filter(public=True, groupcontent__pinned=True).first()
-
-    def get_conversations(self, user):
-        from django.db.models import Max
-        from django.db.models.functions import Coalesce
-        return self.content \
-            .permitted(user) \
-            .filter(article__isnull=False, public=False) \
-            .annotate(last_comment=Max('comments__date_created')) \
-            .annotate(last_activity=Coalesce("last_comment", "date_created")) \
-            .order_by("-last_activity")
-
-    def get_initials(self):
-        import re
-        initials = ''
-        for w in self.name.split():
-            m = re.search('[a-zA-Z0-9]', w)
-            initials += m.group(0) if m else ''
-        return initials
     
 
 class GroupContent(models.Model):
