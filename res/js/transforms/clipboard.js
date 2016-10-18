731fc804524f02759ad44d8c5dfd01e9db46f245
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2016-10-18 15:45:36 +0200

    remove clipboard functionality when not supported

--- res/js/transforms/clipboard.js
+++ res/js/transforms/clipboard.js
@@ -1,7 +1,23 @@
 import Clipboard from "clipboard";
+import browser from "bowser";
 import { success, danger } from "../util/notify";
+import { remove } from "../util/dom";
+
+const is_unsupported = browser.isUnsupportedBrowser({
+    msie: "9",
+    firefox: "41",
+    chrome: "42",
+    edge: "12",
+    opera: "29",
+    safari: "10"
+}, window.navigator.userAgent);
 
 export default (el, opts) => {
+    if(is_unsupported) {
+        remove(el);
+        return;
+    }
+
     const clipboard = new Clipboard(el, {
         text: opts.conf.text
     });
