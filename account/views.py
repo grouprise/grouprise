764eb25190c7f2653b3672d1f3f03e0f422aa666
Lars Kruse <devel@sumpfralle.de>
2017-01-03 19:13:53 +0100

    GPL-Lizensierung: Header in alle relevanten py-Dateien eingefügt

--- account/views.py
+++ account/views.py
@@ -1,3 +1,20 @@
+"""
+This file is part of stadtgestalten.
+
+stadtgestalten is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+stadtgestalten is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero Public License for more details.
+
+You should have received a copy of the GNU Affero Public License
+along with stadtgestalten.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 from . import forms
 from allauth.account import views
 from crispy_forms import layout
