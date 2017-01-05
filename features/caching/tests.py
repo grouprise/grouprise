6fa199183792495c9a4e20646ca655235bc6d4f4
Robert <gestalt@systemausfall.org>
2017-01-05 11:03:10 +0100

    Simplified test code

--- features/caching/tests.py
+++ features/caching/tests.py
@@ -6,19 +6,10 @@ from features.groups import tests as groups
 
 
 class Caching(gestalten.GestaltMixin, groups.GroupMixin, tests.Test):
-    def change_slug(self, slug):
-        self.group.slug = slug
-        self.group.save()
-
-    def test(self):
-        self.disable_caching()
-        self.change_slug('new_slug')
-        ref_response = self.client.get(self.get_url('group-index'))
-
-        self.change_slug('old_slug')
+    def test_group_slug(self):
         self.enable_caching()
-        self.client.get(self.get_url('group-index'))
-        self.change_slug('new_slug')
+        ref_response = self.client.get(self.get_url('group-index'))
+        self.group.slug = 'new_slug'
+        self.group.save()
         response = self.client.get(self.get_url('group-index'))
-
-        self.assertEquals(response.content, ref_response.content)
+        self.assertNotEquals(response.content, ref_response.content)
