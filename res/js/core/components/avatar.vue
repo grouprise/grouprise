b0836f8b66689c247e89ba7711731da8490bfb86
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2017-01-18 17:16:54 +0100

    add avatar component

---
+++ res/js/core/components/avatar.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="avatar-wrap">
+        <div class="avatar" :title="entity.name" :class="avatarClasses" v-if="entity.avatar !== null">
+            <img :src="entity.avatar" alt="">
+        </div>
+        <div class="avatar avatar-initials" :title="entity.name" :class="avatarClasses" :data-initials="entity.initials.length" :style="avatarStyle" v-else>
+            {{ entity.initials }}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            entity: Object,
+            size: {
+                type: Number,
+                default: 48
+            }
+        },
+        computed: {
+            avatarClasses() {
+                return [`avatar-size-${this.size}-${this.size}`]
+            },
+            avatarStyle() {
+                return {
+                    backgroundColor: this.entity.avatar_color
+                }
+            }
+        }
+    }
+</script>
