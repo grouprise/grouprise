cbcf5e45b8dc7e32b2caa32e3b496e4acf449330
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2017-01-18 17:17:16 +0100

    register select and avatar components in vue

--- res/js/setup.js
+++ res/js/setup.js
@@ -1,4 +1,11 @@
 import { $ } from 'luett'
+import Vue from 'vue'
+
+import Select from './core/components/select.vue'
+import Avatar from './core/components/avatar.vue'
+
+Vue.component('sg-select', Select)
+Vue.component('sg-avatar', Avatar)
 
 // load config
 window.app = {
