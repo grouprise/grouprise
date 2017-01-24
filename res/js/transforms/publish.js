aaf7ff7d03335140266a5bf072dce74a8644225a
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2017-01-24 20:32:05 +0100

    add submit listener to publish control

--- res/js/transforms/publish.js
+++ res/js/transforms/publish.js
@@ -1,34 +1,59 @@
-import { $, toggleClass } from 'luett';
+import { $, toggleClass, setAttr } from 'luett'
 import closest from 'closest'
 
-function on(el, event, callback, useCapture = false) {
+function on (el, event, callback, useCapture = false) {
   el.addEventListener(event, callback, useCapture)
   return {
     destroy: () => el.removeEventListener(event, callback, useCapture)
   }
 }
 
-export default el => {
-  const authorSelect = $('[data-select-type="author"]', el);
-  const pinnedCheck = $('input[name="pinned"]', el);
-  const pinnedWrap = closest(pinnedCheck, ".form-group");
+function addAuthorPinnedListener (el) {
+  const authorSelect = $('[data-select-type="author"]', el)
+  const pinnedCheck = $('input[name="pinned"]', el)
+  const pinnedWrap = closest(pinnedCheck, '.form-group')
 
-  if(!authorSelect || !pinnedWrap) {
-    return {
-      remove: () => {}
-    }
-  }
+  if (!authorSelect || !pinnedWrap) return
 
   function setState () {
-    toggleClass(pinnedWrap, "form-group-unused", !authorSelect.value)
+    toggleClass(pinnedWrap, 'form-group-unused', !authorSelect.value)
   }
 
-  const authorSelectListener = on(authorSelect, "change", setState)
+  const authorSelectListener = on(authorSelect, 'change', setState)
   setState()
 
   return {
     remove: () => {
-      authorSelectListener.destroy();
+      authorSelectListener.destroy()
+    }
+  }
+}
+
+function addPublishListener (el) {
+  const form = closest(el, 'form')
+  const submitButton = $('[type="submit"]', el)
+  const submitListener = on(form, 'submit', event => {
+    event.preventDefault()
+    toggleClass(submitButton, 'btn-progress', true)
+    setAttr(submitButton, 'disabled', 'disabled')
+  })
+
+  return {
+    remove: () => {
+      submitListener.destroy()
+    }
+  }
+}
+
+export default el => {
+  const components = [
+    addAuthorPinnedListener(el),
+    addPublishListener(el)
+  ]
+
+  return {
+    remove: () => {
+      components.forEach(cmp => cmp.remove())
     }
   }
 }
