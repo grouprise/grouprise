b84c510cb8bdf429ac9b6d52fc11b2a5b2ed6a1e
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2017-01-26 18:27:14 +0100

    add datetime component

--- res/js/setup.js
+++ res/js/setup.js
@@ -3,11 +3,13 @@ import Vue from 'vue'
 
 import Select from './core/components/select.vue'
 import Avatar from './core/components/avatar.vue'
+import DateTime from './core/components/datetime.vue'
 import NumberSpinner from './core/components/number-spinner.vue'
 import Configurator from './core/components/configurator.vue'
 
 Vue.component('sg-select', Select)
 Vue.component('sg-avatar', Avatar)
+Vue.component('sg-datetime', DateTime)
 Vue.component('sg-number-spinner', NumberSpinner)
 Vue.component('sg-configurator', Configurator)
 
