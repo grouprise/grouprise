149826a319c543966ca7151e4762106a4368ce7f
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2017-01-26 18:26:56 +0100

    add number-spinner component

--- res/js/setup.js
+++ res/js/setup.js
@@ -3,10 +3,12 @@ import Vue from 'vue'
 
 import Select from './core/components/select.vue'
 import Avatar from './core/components/avatar.vue'
+import NumberSpinner from './core/components/number-spinner.vue'
 import Configurator from './core/components/configurator.vue'
 
 Vue.component('sg-select', Select)
 Vue.component('sg-avatar', Avatar)
+Vue.component('sg-number-spinner', NumberSpinner)
 Vue.component('sg-configurator', Configurator)
 
 // load config
