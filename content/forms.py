82b0df6e917fd49bb214b30cf689a1dcfdc6fec7
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2017-02-07 18:51:06 +0100

    fix flake8 error

--- content/forms.py
+++ content/forms.py
@@ -128,7 +128,8 @@ class Event(BaseContent):
 
     class Meta:
         fields = ('all_day', 'author', 'place', 'public', 'text', 'time', 'title', 'until_time')
-        widgets = {'place': forms.Textarea(attrs={'rows': 3, 'data-component': 'autosize', 'data-autosize-rows': 1})}
+        widgets = {'place': forms.Textarea(attrs={'rows': 3, 'data-component': 'autosize',
+                                                  'data-autosize-rows': 1})}
         labels = {'text': 'Beschreibung'}
         model = models.Event
 
