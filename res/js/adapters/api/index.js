3fa88932b20701ff167d6412311e31cdf94e18cd
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2017-02-07 12:54:34 +0100

    add gestalten settings api adapter

--- res/js/adapters/api/index.js
+++ res/js/adapters/api/index.js
@@ -1,9 +1,11 @@
 import Group from './group'
 import Gestalt from './gestalt'
+import GestaltSettings from './gestalt-settings'
 import Image from './image'
 
 export const image = Image()
 export const group = Group()
 export const gestalt = Gestalt()
+export const gestaltSettings = GestaltSettings()
 
-export default ({group, gestalt, image})
+export default ({group, gestalt, image, gestaltSettings})
