8fdc669f471385936eadc80641645c8f2ceefd5a
Robert <gestalt@systemausfall.org>
2017-02-10 14:01:00 +0100

    Finish gestalten index

--- features/gestalten/views.py
+++ features/gestalten/views.py
@@ -5,7 +5,6 @@ from django.views import generic
 
 class List(base.PermissionMixin, generic.ListView):
     permission_required = 'gestalten.view_list'
-    menu = 'gestalt'
     queryset = models.Gestalt.objects.filter(public=True).order_by('-score')
-    template_name = 'entities/gestalt_list.html'
-    title = 'Gestalten'
+    template_name = 'gestalten/list.html'
+    paginate_by = 10
