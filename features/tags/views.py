42e2c983c9a4faafbcf0b20aea4a4e3ddd329f22
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2017-02-10 02:56:46 +0100

    render fallback markup when tag is not found in database

--- features/tags/views.py
+++ features/tags/views.py
@@ -3,6 +3,7 @@ from content import models as content
 from core.views import base
 from django.contrib.contenttypes import models as contenttypes
 from django.core import paginator
+from django.http import Http404
 from django.views import generic
 from features.groups import models as groups
 
@@ -11,6 +12,20 @@ class Tag(base.PermissionMixin, generic.DetailView):
     permission_required = 'tags.view'
     model = models.Tag
     template_name = 'tags/tag.html'
+
+    def get_object(self, queryset=None):
+        try:
+            return super().get_object(queryset)
+        except Http404:
+            slug = self.kwargs.get(self.slug_url_kwarg)
+            tag = models.Tag()
+            tag.name = slug
+            return tag
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['no_data'] = context['object'].pk is None
+        return context
 
     def get_events(self):
         group_ids = self.get_group_tags().values_list('tagged_id', flat=True)
