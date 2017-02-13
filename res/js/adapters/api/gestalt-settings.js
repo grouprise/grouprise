6ef1237c6b8db0182fffdbd9ea9f66bddf140ed0
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2017-02-13 02:18:05 +0100

    auto add csrf token when making post/put/patch/delete requests with json payload

--- res/js/adapters/api/gestalt-settings.js
+++ res/js/adapters/api/gestalt-settings.js
@@ -1,5 +1,4 @@
 import decorator from './_decorator'
-import { cookie } from 'cookie_js'
 
 export default decorator((req, opts) => {
   const endpoint = opts.url(`gestalten/${window.app.conf.gestalt.id}/settings/`)
@@ -9,10 +8,6 @@ export default decorator((req, opts) => {
   }
 
   const create = (data = {}) => {
-    const headers = {
-      'X-CSRFToken': cookie.get('csrftoken')
-    }
-
     return req.post(endpoint, data, { headers })
       .then(res => Promise.resolve(res.data))
   }
