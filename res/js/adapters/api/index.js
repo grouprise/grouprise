8dafedaffb37e8cbaea9babdd45adfa41693a0ad
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2017-02-13 02:21:39 +0100

    add markdown parsing api

--- res/js/adapters/api/index.js
+++ res/js/adapters/api/index.js
@@ -2,10 +2,12 @@ import Group from './group'
 import Gestalt from './gestalt'
 import GestaltSettings from './gestalt-settings'
 import Image from './image'
+import Markdown from './markdown'
 
 export const image = Image()
 export const group = Group()
 export const gestalt = Gestalt()
 export const gestaltSettings = GestaltSettings()
+export const markdown = Markdown()
 
-export default ({group, gestalt, image, gestaltSettings})
+export default ({group, gestalt, image, gestaltSettings, markdown})
---
+++ res/js/adapters/api/markdown.js
@@ -0,0 +1,13 @@
+import { cookie } from 'cookie_js'
+import decorator from './_decorator'
+
+export default decorator((req, opts) => {
+  const endpoint = opts.url('content/markdown/')
+
+  const parse = (content, prefix = 'content') => {
+    return req.post(endpoint, { content, prefix })
+      .then(res => Promise.resolve(res.data.content))
+  }
+
+  return {parse}
+})
