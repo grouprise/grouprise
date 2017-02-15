98c665ed36493d5bcb5a16990c4e51916b74140c
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2017-02-15 23:48:21 +0100

    fix vue alias that caused it to be in dev mode regardless of environment

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
   resolve: {
     alias: {
       app: path.resolve(__dirname, 'res/js'),
-      'vue$': 'vue/dist/vue'
+      'vue$': 'vue/dist/vue.common.js'
     }
   },
   module: {
