968dd5ff8674b6ffbf8a3a686fbe4880b3c364cf
Robert <gestalt@systemausfall.org>
2017-03-03 10:10:48 +0100

    Add In-Reply-To reference to texts

--- features/texts/models.py
+++ features/texts/models.py
@@ -2,6 +2,7 @@ from django.contrib.contenttypes import (
         fields as contenttypes_fields, models as contenttypes_models)
 from django.db import models
 from django.utils import timezone
+import re
 
 
 class ReplyKey(models.Model):
@@ -11,14 +12,26 @@ class ReplyKey(models.Model):
     time_created = models.DateTimeField(auto_now_add=True)
 
 
+class TextManager(models.Manager):
+    def get_by_message_id(self, mid):
+        m = re.match(r'.*\.[0-9]+\.text\.([0-9]+)', mid)
+        if m:
+            return self.get(id=m.group(1))
+        else:
+            raise self.model.DoesNotExist
+
+
 class Text(models.Model):
     container = contenttypes_fields.GenericForeignKey('container_type', 'container_id')
     container_id = models.PositiveIntegerField()
     container_type = models.ForeignKey(contenttypes_models.ContentType)
 
     author = models.ForeignKey('gestalten.Gestalt', related_name='texts')
+    in_reply_to = models.ForeignKey('Text', null=True, related_name='+')
     time_created = models.DateTimeField(default=timezone.now)
     text = models.TextField()
+
+    objects = TextManager()
 
     class Meta:
         ordering = ('time_created',)
--- features/texts/signals.py
+++ features/texts/signals.py
@@ -13,8 +13,16 @@ def process_message(sender, message, **args):
     token_end = len(settings.DEFAULT_REPLY_TO_EMAIL.rsplit('}')[1])
     token = message.to_addresses[0][token_beg:-token_end]
     try:
+        try:
+            in_reply_to_text = models.Text.objects.get_by_message_id(
+                    message.get_email_object().get('In-Reply-To'))
+        except models.Text.DoesNotExist:
+            in_reply_to_text = None
         key = models.ReplyKey.objects.get(key=token)
         models.Text.objects.create(
-                author=key.gestalt, container=key.text.container, text=message.text)
+                author=key.gestalt,
+                container=key.text.container,
+                in_reply_to=in_reply_to_text,
+                text=message.text)
     except models.ReplyKey.DoesNotExist:
         logger.error('Could not process message {}'.format(message.id))
