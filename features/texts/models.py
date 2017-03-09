3053b200b55f07f17e730272c09c639a732faa13
Robert <gestalt@systemausfall.org>
2017-03-09 11:40:07 +0100

    Removed authorship

--- features/texts/models.py
+++ features/texts/models.py
@@ -1,6 +1,7 @@
 from django.contrib.contenttypes import (
         fields as contenttypes_fields, models as contenttypes_models)
 from django.db import models
+from django.utils import timezone
 import re
 
 
@@ -20,29 +21,20 @@ class TextManager(models.Manager):
         raise self.model.DoesNotExist
 
 
-class Authorship(models.Model):
-    author = models.ForeignKey('gestalten.Gestalt')
-    text = models.ForeignKey('Text', related_name='authorships')
-    time_created = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        ordering = ('time_created',)
-
-
 class Text(models.Model):
     container = contenttypes_fields.GenericForeignKey('container_type', 'container_id')
     container_id = models.PositiveIntegerField()
     container_type = models.ForeignKey(contenttypes_models.ContentType)
 
-    authors = models.ManyToManyField(
-            to='gestalten.Gestalt', through='Authorship', related_name='texts')
+    author = models.ForeignKey('gestalten.Gestalt', related_name='texts')
     in_reply_to = models.ForeignKey('Text', null=True, related_name='+')
+    time_created = models.DateTimeField(default=timezone.now)
     text = models.TextField()
 
     objects = TextManager()
 
     class Meta:
-        ordering = ('authorships__time_created',)
+        ordering = ('time_created',)
 
     def get_unique_id(self):
         return '{}.{}.text.{}'.format(self.container_type, self.container.id, self.id)
