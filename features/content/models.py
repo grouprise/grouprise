bd70dfbd5ac247b66aaa55dca96d569f0634fd06
Robert <gestalt@systemausfall.org>
2017-03-10 16:21:11 +0100

    Restructure content

--- features/content/models.py
+++ features/content/models.py
@@ -11,7 +11,7 @@ class Content(models.Model):
             object_id_field='container_id',
             related_query_name='content')
 
-    texts = contenttypes.GenericRelation(
+    contributions = contenttypes.GenericRelation(
             'contributions.Contribution',
             content_type_field='container_type',
             object_id_field='container_id',
@@ -19,3 +19,11 @@ class Content(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Version(models.Model):
+    content = models.ForeignKey('Content', related_name='versions')
+
+    author = models.ForeignKey('gestalten.Gestalt')
+    text = models.TextField()
+    time_created = models.DateTimeField(auto_now_add=True)
