dbd427f1770b2b2a3b181acf4cf997f1abb46fb9
Robert <gestalt@systemausfall.org>
2017-04-06 16:21:28 +0200

    Fix gestalt page

--- stadt/urls.py
+++ stadt/urls.py
@@ -21,7 +21,6 @@ urlpatterns = [
     urls.url(r'^stadt/api/', urls.include('core.api_urls')),
     urls.url(r'^stadt/admin/', admin.site.urls),
     urls.url(r'^$', content_views.ContentList.as_view(), name='index'),
-    urls.url(r'^', urls.include('features.content.urls')),
     urls.url(r'^', urls.include('features.groups.root_urls')),
 
     urls.url(
@@ -29,19 +28,10 @@ urlpatterns = [
         entities_views.Gestalt.as_view(),
         name='gestalt'),
 
-    urls.url(
-        r'^gestalt/(?P<gestalt_slug>[\w.@+-]+)/(?P<slug>[\w-]+)/$',
-        content_views.Content.as_view(),
-        name='gestalt-content'),
+    urls.url(r'^', urls.include('features.content.urls')),
 
     urls.url(
         r'^(?P<group_slug>[\w-]+)/$',
         entities_views.Group.as_view(),
         name='group'),
-
-    urls.url(
-        r'^c/(?P<group_slug>[\w-]+)/(?P<slug>[\w-]+)/$',
-        content_views.Content.as_view(),
-        name='content1'),
-
 ] + static.static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
