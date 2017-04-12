955717cf259bb8996b493a605ce81674d1e1b15d
Lars Kruse <devel@sumpfralle.de>
2017-04-12 18:33:25 +0200

    add calendar export for 'gestalt'

--- features/gestalten/urls.py
+++ features/gestalten/urls.py
@@ -2,8 +2,9 @@ from . import views
 from django.conf import urls
 
 urlpatterns = [
-    urls.url(
-        r'^gestalten/$',
-        views.List.as_view(),
-        name='gestalten'),
+    urls.url(r'^gestalt/(?P<gestalt_slug>[\w.@+-]+)/events/(?P<domain>public|private).ics$',
+             views.CalendarFeed(), name='gestalt-events-feed'),
+    urls.url(r'^gestalten/$',
+             views.List.as_view(),
+             name='gestalten'),
 ]
