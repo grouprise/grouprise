0887bdcb837bf534f472c10af7b9a7751fc40c58
Robert <gestalt@systemausfall.org>
2017-04-27 10:34:23 +0200

    Add intro gallery creation

--- features/galleries/views.py
+++ features/galleries/views.py
@@ -6,3 +6,11 @@ class Create(features.content.views.Create):
     template_name = 'galleries/create.html'
 
     form_class = forms.Create
+
+    def get_initial(self):
+        initial = super().get_initial()
+        initial['title'] = self.request.GET.get('title', initial.get('title'))
+        initial['text'] = self.request.GET.get('text', initial.get('text'))
+        initial['pinned'] = self.request.GET.get('pinned', initial.get('pinned'))
+        initial['public'] = self.request.GET.get('public', initial.get('public'))
+        return initial
