a35c9078d8ececc6de8a56236f2eb069ba22fc94
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2017-05-01 21:50:37 +0200

    restructure settings

---
+++ stadt/settings/__init__.py
@@ -0,0 +1,18 @@
+from os import path, environ
+
+if path.isfile(path.join(path.dirname(__file__), 'local.py')):
+    from stadt.settings.local import *  # noqa: 401
+else:
+    from stadt.settings.default import *  # noqa: 401
+
+try:
+    preset = environ['STADTGESTALTEN_PRESET'].lower()
+
+    if preset == 'development':
+        from stadt.settings.development import *  # noqa: 401
+    elif preset == 'packaging':
+        from stadt.settings.packaging import *  # noqa: 401
+    else:
+        raise ValueError('unknown preset "%s"' % preset)
+except KeyError:
+    pass
---
+++ stadt/settings/packaging.py
@@ -0,0 +1,4 @@
+# this configuration is only loaded during packaging
+# please do not add general app configuration
+
+SECRET_KEY = 'UNSECURE BUILD KEY'
