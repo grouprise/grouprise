6eadb60ca32a87af148781ba09a5b0254b5e1b8c
Robert <gestalt@systemausfall.org>
2017-05-05 14:57:14 +0200

    Flake8 fix

--- features/polls/models.py
+++ features/polls/models.py
@@ -13,7 +13,7 @@ class Option(core.models.Model):
 
 class Vote(core.models.Model):
     option = models.ForeignKey('Option')
-    
+
     voter = models.ForeignKey('gestalten.Gestalt', null=True)
     anonymous = models.CharField(max_length=63, blank=True)
 
