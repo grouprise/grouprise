c47c771b7c5a1b34c6122809dcac68b66bca3afd
Robert <gestalt@systemausfall.org>
2017-05-05 11:37:56 +0200

    Add options formset to create form

--- features/polls/forms.py
+++ features/polls/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 
 from features.content import forms as content
+from . import models
+
+
+OptionFormSet = forms.modelformset_factory(
+        models.Option, extra=3, fields=('title',), labels={'title': 'Antwort'})
 
 
 class Create(content.Create):
-    text = forms.CharField(label='Beschreibung', widget=forms.Textarea({'rows': 2}))
+    text = forms.CharField(label='Beschreibung / Frage', widget=forms.Textarea({'rows': 2}))
+
+    options = OptionFormSet()
