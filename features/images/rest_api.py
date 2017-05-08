bfb60561f30422701613ea30e7cc952915193492
Robert <gestalt@systemausfall.org>
2017-05-08 12:55:11 +0200

    Fix image api

--- features/images/rest_api.py
+++ features/images/rest_api.py
@@ -3,10 +3,14 @@ from rest_framework import viewsets, mixins, serializers
 
 from core import api
 from content import models as content_models
-from features.images import models as images
+from . import models
 
 
 class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Image
+        fields = ('id', 'file', 'title', 'creator', 'path')
+
     title = serializers.CharField(source='file.name', read_only=True)
     path = serializers.CharField(source='file.url', read_only=True)
 
@@ -24,40 +28,26 @@ class ImageSerializer(serializers.ModelSerializer):
         validated_data.update({"creator": self._get_gestalt()})
         return super().update(instance, validated_data)
 
-    class Meta:
-        model = images.Image
-        fields = ('id', 'file', 'weight', 'content', 'title', 'creator', 'path')
-
 
 class ImageSet(mixins.CreateModelMixin, mixins.UpdateModelMixin, viewsets.ReadOnlyModelViewSet):
     serializer_class = ImageSerializer
-    filter_fields = ('content', 'creator', )
+    filter_fields = ('creator',)
 
     def get_queryset(self):
         user = self.request.user
-        content = content_models.Content.objects.permitted(user)
-        try:
-            gestalt = user.gestalt
-        except AttributeError:
-            gestalt = None
-        return content_models.Image.objects.filter(
-            Q(content__in=content) | Q(creator=gestalt)).order_by('-weight')
+        return models.Image.objects.filter(creator__user=user)
 
     def has_permission(self):
         if self.action == 'create':
-            content_pk = self.request.data.get('content')
-            if content_pk:
-                content = content_models.Content.objects.get(pk=content_pk)
-                return self.request.user.has_perm('content.create_image', content)
             return True
         elif self.action == 'list':
             return True
         elif self.action == 'retrieve':
             image = self.get_object()
-            return self.request.user.has_perm('content.view_image', image)
-        elif self.action == 'update':
-            image = self.get_object()
-            return self.request.user.has_perm('content.update_image', image)
+            return self.request.user.has_perm('images.view', image)
+        # elif self.action == 'update':
+        #     image = self.get_object()
+        #     return self.request.user.has_perm('content.update_image', image)
         return False
 
 
---
+++ features/images/rules.py
@@ -0,0 +1,7 @@
+import rules
+
+@rules.predicate
+def is_creator(user, image):
+    return user == image.creator.user
+
+rules.add_perm('images.view', is_creator)
