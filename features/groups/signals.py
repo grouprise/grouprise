483d19a0a6f579b8c04ca09b799ae24acaf29c3a
Robert <gestalt@systemausfall.org>
2017-06-07 13:54:31 +0200

    Show address and site only when set explicitly

--- features/groups/signals.py
+++ features/groups/signals.py
@@ -7,16 +7,8 @@ from django.core import urlresolvers
 
 
 def set_group_meta(group):
-    if not group.address:
-        group.address = group.name
     if not group.date_founded:
         group.date_founded = date.today()
-    if not group.url:
-        group.url = '{protocol}://{domain}{path}'.format(
-                protocol=settings.ACCOUNT_DEFAULT_HTTP_PROTOCOL,
-                domain=sites_models.Site.objects.get_current().domain,
-                path=urlresolvers.reverse('group', args=[group.slug]),
-                )
     group.save()
 
 
