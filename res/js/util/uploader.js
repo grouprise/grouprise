ccc381b9cf1718c65720aa240b1de2c545a7d7fe
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2017-07-17 18:17:09 +0200

    allow callee to determine how the uploader should resolve the uploads

--- res/js/util/uploader.js
+++ res/js/util/uploader.js
@@ -1,4 +1,5 @@
-import { sum } from 'lodash'
+import { sum, isFunction } from 'lodash'
+import Q from 'q'
 
 function progressAdapterFactory (onProgressCallback) {
   const iface = {}
@@ -26,13 +27,27 @@ function progressAdapterFactory (onProgressCallback) {
   return iface
 }
 
+function selectResolver (spec) {
+  switch (true) {
+    case spec === 'all':
+      return Q.all
+    case spec === 'allSettled':
+      return Q.allSettled
+    case isFunction(spec):
+      return spec
+    default:
+      throw Error('unknown resolver', spec)
+  }
+}
+
 export default function UploaderFactory (adapter) {
-  function upload (files, onProgress) {
+  function upload (files, onProgress, resolveWith = 'all') {
+    const resolver = selectResolver(resolveWith)
     const progressAdapter = progressAdapterFactory(onProgress)
     const uploads = files.map(file => {
       return adapter.create({file}, {onProgress: progressAdapter.createHandler()})
     })
-    return Promise.all(uploads)
+    return resolver(uploads)
   }
 
   return { upload }
