c9cfb10e5c3e6a910efb4297a0ce278bf067651c
Robert <gestalt@systemausfall.org>
2017-07-24 16:12:47 +0200

    Add basic index and group feeds

---
+++ features/stadt/feeds.py
@@ -0,0 +1,36 @@
+import django
+
+from features.associations import models as associations
+from features.groups import models as groups
+
+
+class Index(django.contrib.syndication.views.Feed):
+    link = django.core.urlresolvers.reverse_lazy('index')
+    description_template = 'feeds/detail.html'
+
+    def items(self):
+        return associations.Association.objects.ordered_user_content(
+                django.contrib.auth.models.AnonymousUser())
+
+    def item_title(self, item):
+        return str(item)
+
+    def item_pubdate(self, item):
+        return item.container.versions.first().time_created
+
+    def title(self):
+        return django.contrib.sites.models.Site.objects.get_current().name
+
+
+class Group(Index):
+    def get_object(self, request, group_pk):
+        return django.shortcuts.get_object_or_404(groups.Group, pk=group_pk)
+
+    def items(self, obj):
+        return super().items().filter(group=obj)
+
+    def link(self, obj):
+        return obj.get_absolute_url()
+
+    def title(self, obj):
+        return str(obj)
--- features/stadt/urls.py
+++ features/stadt/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 
 from features.content import views as content
-from . import views
+from . import feeds, views
 
 urlpatterns = [
     url(
@@ -10,10 +10,27 @@ urlpatterns = [
         name='index'),
 
     url(
+        r'^stadt/feed/$',
+        feeds.Index(),
+        name='feed'),
+
+    url(
+        r'^stadt/groups/(?P<group_pk>[0-9]+)/feed/$',
+        feeds.Group(),
+        name='group-feed'),
+
+    url(
+        r'^stadt/imprint/$',
+        views.Imprint.as_view(),
+        name='imprint'),
+
+    url(
+        r'^stadt/privacy/$',
+        views.Privacy.as_view(),
+        name='privacy'),
+
+    url(
         r'^(?P<entity_slug>[\w.@+-]+)/$',
         views.Entity.as_view(),
         name='entity'),
-
-    url(r'^imprint/$', views.Imprint.as_view(), name='imprint'),
-    url(r'^privacy/$', views.Privacy.as_view(), name='privacy'),
 ]
