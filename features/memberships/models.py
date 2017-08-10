6620032e9f8574c3e1dad37c111040eca570a751
Robert <gestalt@systemausfall.org>
2017-08-10 11:30:46 +0200

    Add queryset for ordering memberships by activity

--- features/memberships/models.py
+++ features/memberships/models.py
@@ -1,22 +1,26 @@
 from django.contrib.contenttypes import fields as contenttypes
 from django.db import models
 
+from . import querysets
+
 
 class Membership(models.Model):
+    class Meta:
+        unique_together = ('group', 'member')
+
     created_by = models.ForeignKey(
             'gestalten.Gestalt', related_name='memberships_created')
     date_joined = models.DateField(auto_now_add=True)
     group = models.ForeignKey('groups.Group', related_name='memberships')
     member = models.ForeignKey('gestalten.Gestalt', related_name='memberships')
 
+    objects = models.Manager.from_queryset(querysets.MembershipQuerySet)()
+
     def __str__(self):
         return "%s is member of %s since %s" % (
             str(self.member.user.get_username()),
             str(self.group.slug), str(self.date_joined)
         )
-
-    class Meta:
-        unique_together = ('group', 'member')
 
 
 class Application(models.Model):
