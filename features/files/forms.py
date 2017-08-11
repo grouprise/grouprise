2417e54ca1c82b5ac237e261227a676c6e074afc
Robert <gestalt@systemausfall.org>
2017-08-11 17:00:55 +0200

    Add file size hint

--- features/files/forms.py
+++ features/files/forms.py
@@ -1,5 +1,6 @@
 import os
 
+import django
 from django import forms
 
 from features.content import forms as content
@@ -7,7 +8,9 @@ from features.content import forms as content
 
 class Create(content.Create):
     text = forms.CharField(label='Beschreibung', widget=forms.Textarea({'rows': 2}))
-    file = forms.FileField(label='Datei')
+    file = forms.FileField(
+            label='Datei', help_text='Die maximal erlaubte Dateigröße beträgt {} MB.'.format(
+                django.conf.settings.MAX_FILE_SIZE // (1024 * 1024)))
 
     def save(self, commit=True):
         association = super().save(False)
