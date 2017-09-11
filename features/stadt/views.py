1fa7237f47096bc9574ffdf649e6231bf2a670e9
Robert <gestalt@systemausfall.org>
2017-09-11 12:16:13 +0200

    Fix permission checking for entity proxy view

--- features/stadt/views.py
+++ features/stadt/views.py
@@ -1,22 +1,32 @@
 import django
 
+import core
 import utils
 from features import gestalten, groups
 
 
-class Entity(django.views.generic.View):
+class Entity(core.views.PermissionMixin, django.views.generic.View):
     def get(self, request, *args, **kwargs):
+        return self.view.get(request, *args, **kwargs)
+
+    def get_view(self):
+        entity_slug = self.kwargs.get('entity_slug')
         try:
-            entity = groups.models.Group.objects.get(slug=kwargs.get('entity_slug'))
+            entity = groups.models.Group.objects.get(slug=entity_slug)
             view = groups.views.Group()
+            view.related_object = entity
         except groups.models.Group.DoesNotExist:
             entity = django.shortcuts.get_object_or_404(
-                    gestalten.models.Gestalt, user__username=kwargs.get('entity_slug'))
+                    gestalten.models.Gestalt, user__username=entity_slug)
             view = gestalten.views.Detail()
-        view.request = request
-        view.kwargs = kwargs
-        view.object = entity
-        return view.get(request, *args, **kwargs)
+            view.object = entity
+        return view
+
+    def has_permission(self):
+        self.view = self.get_view()
+        self.view.request = self.request
+        self.view.kwargs = self.kwargs
+        return self.view.has_permission()
 
 
 class Imprint(utils.views.PageMixin, django.views.generic.TemplateView):
