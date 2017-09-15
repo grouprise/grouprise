9c6cacae251244bdcd00926723b205a0f9b9057e
Robert <gestalt@systemausfall.org>
2017-09-15 08:45:01 +0200

    Finish note creation form/ view

--- features/board/models.py
+++ features/board/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Note(models.Model):
     related_to = models.ForeignKey('associations.Association', on_delete=models.CASCADE)
-    text = models.CharField(max_length=70)
+    text = models.CharField(max_length=70, help_text='höchstens 70 Zeichen')
     time_created = models.DateTimeField(auto_now_add=True)
     gestalt_created = models.ForeignKey(
             'gestalten.Gestalt', on_delete=models.SET_NULL, null=True)
--- features/board/views.py
+++ features/board/views.py
@@ -2,16 +2,27 @@ import django
 
 import core
 import features
-from . import forms
+from . import forms, models
 
 
 class Create(
         core.views.PermissionMixin, features.associations.views.AssociationMixin,
-        django.views.generic.CreateView):
+        django.contrib.messages.views.SuccessMessageMixin, django.views.generic.CreateView):
     permission_required = 'board.create'
     form_class = forms.Create
     template_name = 'board/create.html'
+    success_message = 'Notiz wurde am "Schwarzen Brett" auf der Startseite angeheftet.'
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['instance'] = models.Note(related_to=self.association)
+        if self.request.user.is_authenticated():
+            kwargs['instance'].gestalt_created = self.request.user.gestalt
+        return kwargs
 
     def get_permission_object(self):
         self.association = self.get_association()
         return self.association
+
+    def get_success_url(self):
+        return self.association.get_absolute_url()
