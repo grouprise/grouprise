d4b3ecc367d56a9f8cdadc140510496391c65a4b
Robert <gestalt@systemausfall.org>
2017-09-15 09:16:07 +0200

    Add board to index page

---
+++ features/board/templatetags/board.py
@@ -0,0 +1,9 @@
+from django import template
+
+from features.board.models import Note
+
+register = template.Library()
+
+@register.simple_tag
+def get_notes(num_max):
+    return Note.objects.order_by('-time_created')[:num_max]
