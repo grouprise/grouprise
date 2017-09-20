1b674c11df048977fc2901cd31c9baf1e9be540b
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2017-09-21 01:16:13 +0200

    use popper for positioning group-preview. refs #377

--- res/js/components/content/group-preview.vue
+++ res/js/components/content/group-preview.vue
@@ -1,24 +1,56 @@
 <template>
-  <div class="group-preview">
-    <header class="group-preview-header">
-      <div class="group-preview-image" :class="{'group-preview-image-placeholder': !group.cover}">
-        <img :src="group.cover" v-if="group.cover" alt="">
+  <transition name="fade-down">
+    <div class="group-preview" v-show="showPreview"
+         @mouseenter="setHovered(true)" @mouseleave="setHovered(false)">
+      <header class="group-preview-header">
+        <div class="group-preview-image" :class="{'group-preview-image-placeholder': !group.cover}">
+          <img :src="group.cover" v-if="group.cover" alt="">
+        </div>
+        <div class="avatar-wrap group-avatar">
+          <sg-avatar :size="64" :entity="group"></sg-avatar>
+        </div>
+      </header>
+      <div class="group-preview-body">
+        <h3>{{ group.name }}</h3>
+        <p>{{ group.description }}</p>
       </div>
-      <div class="avatar-wrap group-avatar">
-        <sg-avatar :size="64" :entity="group"></sg-avatar>
-      </div>
-    </header>
-    <div class="group-preview-body">
-      <h3>{{ group.name }}</h3>
-      <p>{{ group.description }}</p>
     </div>
-  </div>
+  </transition>
 </template>
 
 <script>
   export default {
+    data () {
+      return {
+        isHovered: false,
+        isHoveredDelay: null
+      }
+    },
     props: {
-      group: Object
+      group: Object,
+      visibilityDelay: {
+        type: Number,
+        default: 250
+      },
+      visible: {
+        type: Boolean,
+        default: true
+      }
+    },
+    computed: {
+      showPreview () {
+        return this.visible || this.isHovered
+      }
+    },
+    methods: {
+      setHovered (value) {
+        if (value) {
+          clearTimeout(this.isHoveredDelay)
+          this.isHovered = true
+        } else {
+          this.isHoveredDelay = setTimeout(() => { this.isHovered = false }, this.visibilityDelay)
+        }
+      }
     }
   }
 </script>
