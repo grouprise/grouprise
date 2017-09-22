2fd8039cfedb23b9139f03d2e72726f0ff7389f8
Robert <gestalt@systemausfall.org>
2017-09-22 14:29:14 +0200

    Add delete link to contributions

---
+++ features/contributions/rules.py
@@ -0,0 +1,7 @@
+from rules import add_perm, is_authenticated, predicate
+
+@predicate
+def is_creator(user, contribution):
+    return contribution.author == user.gestalt
+
+add_perm('contributions.delete', is_authenticated & is_creator)
