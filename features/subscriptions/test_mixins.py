d7d8926acf7000665d46593aaa0f53a0086958e2
Robert <gestalt@systemausfall.org>
2017-10-04 14:16:47 +0200

    Remove obsolete test mixins

--- features/subscriptions/test_mixins.py
+++ features/subscriptions/test_mixins.py
@@ -1,7 +1,6 @@
-from . import filters, models
+from . import models
 from features.gestalten import tests as gestalten
 from features.groups import tests as groups
-from features.memberships import test_mixins as memberships
 
 
 class GroupSubscribedMixin(gestalten.AuthenticatedMixin, groups.GroupMixin):
@@ -20,23 +19,3 @@ class OtherGroupSubscriberMixin(
         models.Subscription.objects.create(
                 subscribed_to=cls.group,
                 subscriber=cls.other_gestalt)
-
-
-class AllContentUnsubscribedMixin(memberships.MemberMixin):
-    @classmethod
-    def setUpTestData(cls):
-        super().setUpTestData()
-        s = models.Subscription.objects.create(
-                subscribed_to=cls.group, subscriber=cls.gestalt, unsubscribe=True)
-        models.Filter.objects.create(
-                filter_id=filters.all_content.filter_id, subscription=s)
-
-
-class ExternalUnsubscribedMixin(memberships.MemberMixin):
-    @classmethod
-    def setUpTestData(cls):
-        super().setUpTestData()
-        s = models.Subscription.objects.create(
-                subscribed_to=cls.group, subscriber=cls.gestalt, unsubscribe=True)
-        models.Filter.objects.create(
-                filter_id=filters.initial_author_no_member.filter_id, subscription=s)
