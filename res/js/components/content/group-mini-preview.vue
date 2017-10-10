f6660529b9868312ddec9ba9b0036e088315ac70
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2017-10-10 06:07:47 +0200

    add views for group mini preview similar to profiles as django template and vue component

---
+++ res/js/components/content/group-mini-preview.vue
@@ -0,0 +1,19 @@
+<template>
+  <a class="group-media" :href="group.url">
+    <header class="group-media-image">
+      <sg-avatar :size="64" :entity="group" />
+    </header>
+    <div class="group-media-body">
+      <h3>{{ group.name }}</h3>
+      <p>{{ group.description|default("Diese Gruppe hat noch keine Beschreibung auf Stadtgestalten hinterlegt.")|truncatewords(10) }}</p>
+    </div>
+  </a>
+</template>
+
+<script>
+  export default {
+    props: {
+      group: Object
+    }
+  }
+</script>
