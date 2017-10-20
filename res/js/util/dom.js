06696fbf25215bc21a5f3d24d2db9022058846c1
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2017-10-21 01:09:27 +0200

    fix time parsing bug in polls

--- res/js/util/dom.js
+++ res/js/util/dom.js
@@ -36,7 +36,29 @@ export function createContainerAdapter (el) {
   }
 }
 
+export function getTimeFormat (date) {
+  // todo there might be a better way to do this :)
+  // detect the date-format based on the current value
+  for (const [check, format] of getTimeFormat.formats) {
+    if (check.test(date.value)) {
+      getTimeFormat.default = format
+      return format
+    }
+  }
+
+  console.error(`unknown timeformat for date ${date.value}`)
+  return getTimeFormat.default
+}
+
+getTimeFormat.formats = [
+  [/^[\d]{4}-[\d]{2}-[\d]{2}/, 'YYYY-MM-DD HH:mm:ss'],
+  [/^[\d]{2}\.[\d]{2}\.[\d]{4}/, 'DD.MM.YYYY HH:mm:ss']
+]
+getTimeFormat.default = getTimeFormat.formats[0][1]
+getTimeFormat.toString = () => getTimeFormat.default
+
 export default {
   createDisplayControl,
-  createContainerAdapter
+  createContainerAdapter,
+  getTimeFormat
 }
