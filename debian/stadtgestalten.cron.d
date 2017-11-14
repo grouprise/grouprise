6eec392ec150b6b7b60eb093ab2b8b01e59350b7
Lars Kruse <devel@sumpfralle.de>
2017-11-14 20:42:33 +0100

    Logging nach /var/log/stadtgestalten/ umgelenkt

--- debian/stadtgestalten.cron.d
+++ debian/stadtgestalten.cron.d
@@ -11,5 +11,5 @@
 46  * * * * root    stadtctl import_feeds
 
 # send mails with django-mailer
-*   * * * * root    stadtctl send_mail >>/var/lib/stadtgestalten/mailer.log 2>&1
-9,29,49 * * * * root stadtctl retry_deferred >>/var/lib/stadtgestalten/mailer.log 2>&1
+*   * * * * root    stadtctl send_mail >>/var/log/stadtgestalten/mailer-stadtgestalten.log 2>&1
+9,29,49 * * * * root stadtctl retry_deferred >>/var/log/stadtgestalten/mailer-stadtgestalten.log 2>&1
