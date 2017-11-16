378291a1be52f8ffbf4d89f295eaf799448f3d95
Robert <gestalt@systemausfall.org>
2017-11-16 16:08:16 +0100

    Move utils auth to gestalten

--- features/events/views.py
+++ features/events/views.py
@@ -14,9 +14,9 @@ import features.content.views
 import features.groups.views
 from features.associations import models as associations
 from features.gestalten import models as gestalten
+from features.gestalten.auth import get_user_resolver
 from features.memberships.predicates import is_member_of
 from utils import views as utils_views
-from utils.auth import get_user_resolver
 from .utils import get_requested_time
 from . import forms
 
