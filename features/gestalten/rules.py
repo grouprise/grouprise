cd0eb7e78b3819bf48048e38efde0436736b844d
Robert <gestalt@systemausfall.org>
2017-11-16 16:49:52 +0100

    Move account.rules to features.gestalten

--- features/gestalten/rules.py
+++ features/gestalten/rules.py
@@ -19,3 +19,11 @@ rules.add_perm('entities.change_gestalt', rules.is_authenticated & is_gestalt)
 rules.add_perm('gestalten.view_list', rules.always_allow)
 
 rules.add_perm('gestalten.login', ~rules.is_authenticated)
+
+rules.add_perm('account.change_password', rules.is_authenticated)
+rules.add_perm('account.confirm', rules.always_allow)
+rules.add_perm('account.email', rules.is_authenticated)
+rules.add_perm('account.logout', rules.is_authenticated)
+rules.add_perm('account.reset_password', ~rules.is_authenticated)
+rules.add_perm('account.set_password', rules.is_authenticated)
+rules.add_perm('account.signup', ~rules.is_authenticated)
