7a60b98b5106799c9441079c679968679f61edef
Robert <gestalt@systemausfall.org>
2017-11-17 14:31:01 +0100

    Remove sharing fragments and recommend box from sidebar

--- features/sharing/fragments.py
+++
@@ -1,8 +0,0 @@
-from core import fragments
-
-fragments.register(
-        'group-meta-subscriptions-share', 'sharing/_group_meta_share.html')
-
-fragments.insert(
-        'group-meta-subscriptions-share',
-        'group-meta-subscriptions')
