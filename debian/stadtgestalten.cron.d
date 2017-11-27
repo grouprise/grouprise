81b8fce0f2a696f3bdf43786a878e199152b3904
Robert <gestalt@systemausfall.org>
2017-11-27 10:38:02 +0100

    Change to /tmp before sending mail

--- debian/stadtgestalten.cron.d
+++ debian/stadtgestalten.cron.d
@@ -11,5 +11,5 @@
 46  * * * * root    stadtctl import_feeds
 
 # send mails with django-mailer
-*   * * * * root    stadtctl send_mail >>/var/log/stadtgestalten/mailer-stadtgestalten.log 2>&1
-9,29,49 * * * * root stadtctl retry_deferred >>/var/log/stadtgestalten/mailer-stadtgestalten.log 2>&1
+*   * * * * root    cd /tmp && stadtctl send_mail >>/var/log/stadtgestalten/mailer-stadtgestalten.log 2>&1
+9,29,49 * * * * root cd /tmp && stadtctl retry_deferred >>/var/log/stadtgestalten/mailer-stadtgestalten.log 2>&1
