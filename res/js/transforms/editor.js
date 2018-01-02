984212a2631715d50abba4d09766767caa54f104
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2018-01-02 16:17:57 +0100

    refactor editor transform to improve code clarity and documentation

--- res/js/transforms/editor.js
+++ res/js/transforms/editor.js
@@ -1,22 +1,16 @@
-import { $, setAttr, hasAttr, on } from 'luett'
+import { setAttr, hasAttr, on } from 'luett'
 import Drop from 'tether-drop'
 import bel from 'bel'
 import closest from 'closest'
 import { throttle } from 'lodash'
 
-import { markdown } from '../adapters/api'
 import { EVENT_CITE } from './cite'
 import ImageEditor from '../components/image/editor-image'
+import createOptions from './editor.options'
 
 const HEADER_OFFSET = 60
 const imageEditor = ImageEditor()
 const imageDialog = bel`<div class="editor-dialog">${imageEditor.el}</div>`
-
-function quote (text) {
-  return text.split('\n')
-        .map(line => `> ${line}`)
-        .join('\n')
-}
 
 function createStandardEventEmitter (obj) {
   return {
@@ -25,137 +19,29 @@ function createStandardEventEmitter (obj) {
   }
 }
 
+function quote (text) {
+  return text.split('\n')
+        .map(line => `> ${line}`)
+        .join('\n')
+}
+
 function editor (CodeMirror, SimpleMDE, el, opts) {
+  // force the codemirrors inputStyle to textarea, to workaround
+  // contentEditable bugs on mobile devices
   CodeMirror.defaults.inputStyle = 'textarea'
 
+  // find editor container. we rely on existing markup here. maybe it’s a
+  // good idea to create this one here
   const containerEl = closest(el, '.editor-container')
   containerEl.classList.add('editor-container-ready')
 
   const { bus } = opts.conf
   const isRequired = hasAttr(el, 'required')
   const form = closest(el, 'form')
-  const editor = new SimpleMDE({
-    autoDownloadFontAwesome: false,
-    element: el,
-    forceSync: true,
-    parsingConfig: {
-      allowAtxHeaderWithoutSpace: true,
-      strikethrough: false
-    },
-    promptTexts: {
-      link: 'Adresse des Linkziels (URL):',
-      image: 'Adresse des Bildes (URL):'
-    },
-    promptURLs: true,
-    shortcuts: {
-      'toggleBold': 'Ctrl-B',
-      'toggleItalic': 'Ctrl-I',
-      'undo': 'Ctrl-Z',
-      'redo': 'Ctrl-Y',
-      'toggleUnorderedList': 'Shift-F12',
-      'toggleOrderedList': 'F12',
-      'toggleHeading2': 'Ctrl-2',
-      'toggleHeading3': 'Ctrl-3'
-    },
-    spellChecker: false,
-    status: false,
-    tabSize: 4,
-    previewRender: function (plainText, preview) {
-      markdown.parse(plainText)
-        .then(
-          content => { preview.innerHTML = `<div class="content-body">${content}</div>` },
-          () => { preview.innerHTML = `<p class="disclaimer">Fehler beim Laden der Vorschau</p>` }
-        )
+  const editor = new SimpleMDE(createOptions(el, SimpleMDE))
 
-      return 'Lade Vorschau...'
-    },
-    toolbar: [
-      {
-        name: 'undo',
-        action: SimpleMDE.undo,
-        className: 'fa fa-undo no-disable',
-        title: 'Rückgängig'
-      },
-      {
-        name: 'redo',
-        action: SimpleMDE.redo,
-        className: 'fa fa-repeat no-disable',
-        title: 'Wiederholen'
-      },
-      '|',
-      {
-        name: 'italic',
-        action: SimpleMDE.toggleItalic,
-        className: 'fa fa-italic',
-        title: 'Kursiv'
-      },
-      {
-        name: 'bold',
-        action: SimpleMDE.toggleBold,
-        className: 'fa fa-bold',
-        title: 'Fett'
-      },
-      '|',
-      {
-        name: 'blockquote',
-        action: SimpleMDE.toggleBlockquote,
-        className: 'fa fa-quote-left',
-        title: 'Zitat'
-
-      },
-      {
-        name: 'unordered-list',
-        action: SimpleMDE.toggleUnorderedList,
-        className: 'fa fa-list-ul',
-        title: 'Einfache Liste'
-      },
-      {
-        name: 'ordered-list',
-        action: SimpleMDE.toggleOrderedList,
-        className: 'fa fa-list-ol',
-        title: 'Nummerierte Liste'
-      },
-      '|',
-      {
-        name: 'heading-1',
-        action: SimpleMDE.toggleHeading1,
-        className: 'fa fa-header',
-        title: 'Überschrift'
-      },
-      {
-        name: 'heading-2',
-        action: SimpleMDE.toggleHeading2,
-        className: 'fa fa-header fa-header-x fa-header-2',
-        title: 'Unterüberschrift'
-      },
-      '|',
-      {
-        name: 'link',
-        action: SimpleMDE.drawLink,
-        className: 'fa fa-link',
-        title: 'Link/Verweis'
-      },
-      {
-        name: 'image',
-        className: 'fa fa-picture-o',
-        title: 'Bild'
-      },
-      '|',
-      {
-        name: 'preview',
-        action: SimpleMDE.togglePreview,
-        className: 'sg sg-preview no-disable',
-        title: 'Vorschau'
-      },
-      {
-        name: 'guide',
-        action: '/stadt/markdown',
-        className: 'fa fa-question-circle',
-        title: 'Möglichkeiten der Textauszeichnung'
-      }
-    ]
-  })
-
+  // create our drop container for images
+  // TODO this should be replaced with popper.js
   const drop = new Drop({
     target: editor.toolbarElements['image'],
     content: imageDialog,
@@ -188,11 +74,15 @@ function editor (CodeMirror, SimpleMDE, el, opts) {
     toolbar.style.transform = offset > 0 ? `translateY(${offset}px)` : ''
   }, 250), { passive: true })
 
+  // create focusListener to move the toolbar into the current scrollPosition
+  // whenever the editor is focused
   const focusListener = on(codemirrorEvents, ['focus', 'blur'], () => {
     containerEl.classList.toggle('editor-container-active', editor.codemirror.hasFocus())
   })
 
-  imageEditor.emitter.on('files:select', (files) => {
+  // whenever images have been selected and uploaded insert image
+  // markdown image references into the editor
+  imageEditor.emitter.on('files:select', files => {
     const text = editor.codemirror.getSelection()
     const code = files.map((file) => {
       return `![${(text.length > 0) ? text : file.label}](${file.content})`
@@ -202,16 +92,25 @@ function editor (CodeMirror, SimpleMDE, el, opts) {
     drop.close()
   })
 
-  imageEditor.emitter.on('files:add', (files) => {
+  // whenever images have been added, append hidden `images` fields for
+  // to the form. this way we associate content with images
+  // TODO are we still assigning images to content in any way?
+  imageEditor.emitter.on('files:add', files => {
     files.forEach((file) => {
       form.appendChild(bel`<input type="hidden" name="images" value="${file.id}">`)
     })
   })
 
+  // when someone clicks on a cite link an event is send through our
+  // application bus. we use this event to place the cited content
+  // into our editor with markdown quotes
   const citeListener = bus.on(EVENT_CITE, data => {
     editor.codemirror.doc.replaceSelection(quote(data.formattedText))
   })
 
+  // we remove the required attribute from the original textarea
+  // because otherwise the browser’s html5 validation might might chip in
+  // when the form is submitted and would prevent the form from being sent
   if (isRequired) {
     el.removeAttribute('required')
   }
