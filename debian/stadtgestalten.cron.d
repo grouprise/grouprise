415072a35d8475f51acd548aad7a18b8550fd025
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2018-01-04 23:34:12 +0100

    optimize cron file with use of moreutils/chronic

--- debian/stadtgestalten.cron.d
+++ debian/stadtgestalten.cron.d
@@ -2,14 +2,14 @@
 #    MAILTO=admin@example.org
 
 # process incoming mails
-*/3 * * * * root    stadtctl getmail 2>&1 | grep -v ^INFO:
+*/3 * * * * root        chronic stadtctl getmail
 
 # calculate scores for groups and users
-23  * * * * root    stadtctl update_scores
+23  * * * * root        chronic stadtctl update_scores
 
 # import content from external websites
-46  * * * * root    stadtctl import_feeds
+46  * * * * root        chronic stadtctl import_feeds
 
 # send mails with django-mailer
-*   * * * * root    cd /tmp && stadtctl send_mail >>/var/log/stadtgestalten/mailer-stadtgestalten.log 2>&1
-9,29,49 * * * * root cd /tmp && stadtctl retry_deferred >>/var/log/stadtgestalten/mailer-stadtgestalten.log 2>&1
+*   * * * * root        cd /tmp && chronic sh -c "stadtctl send_mail 2>&1 | tee -a /var/log/stadtgestalten/mailer-stadtgestalten.log"
+9,29,49 * * * * root    cd /tmp && chronic sh -c "stadtctl retry_deferred 2>&1 | tee -a /var/log/stadtgestalten/mailer-stadtgestalten.log"
