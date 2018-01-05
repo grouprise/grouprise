48a3d4716228e5107508e68bce356b140037175a
Robert <gestalt@systemausfall.org>
2018-01-05 11:29:53 +0100

    Fix simple vote display

--- features/polls/views.py
+++ features/polls/views.py
@@ -16,43 +16,17 @@ class Create(features.content.views.Create):
 class Detail(features.content.views.DetailBase):
     template_name = 'polls/detail.html'
 
-    def get_winner(self, vote_count: dict):
-        result = []
-        for option, votes in vote_count.items():
-            result.append(
-                (option, votes['yes'] + votes['maybe'] * .33334)
-            )
-        try:
-            return sorted(result, key=lambda item: item[1], reverse=True)[0][0]
-        except IndexError:
-            return None
-
     def get_context_data(self, **kwargs):
-        def vote_key(endorse):
-            if endorse is None:
-                return 'maybe'
-            elif endorse:
-                return 'yes'
-            else:
-                return 'no'
-
         # options
         kwargs['options'] = self.object.container.poll.options.order_by('id')
 
         # votes
-        votes = models.SimpleVote.objects.filter(option__poll=self.object.container.poll).order_by(
-                'time_updated')
-        votes_dict = collections.defaultdict(dict)
-        vote_count = collections.defaultdict(lambda: dict(yes=0, no=0, maybe=0))
-        for vote in votes:
-            vote_count[vote.option][vote_key(vote.endorse)] += 1
-            if vote.voter:
-                votes_dict[vote.voter][vote.option] = vote
-                votes_dict[vote.voter]['latest'] = vote
-            else:
-                votes_dict[vote.anonymous][vote.option] = vote
-                votes_dict[vote.anonymous]['latest'] = vote
-        kwargs['votes'] = votes_dict
+        votes = models.Vote.objects.filter(
+                option__poll=self.object.container.poll).order_by('time_updated')
+        if self.object.container.poll.condorcet:
+            kwargs.update(self.get_condorcet_votes(votes))
+        else:
+            kwargs.update(self.get_simple_votes(votes))
 
         # voters
         voters = []
@@ -66,12 +40,50 @@ class Detail(features.content.views.DetailBase):
         # vote form
         vote_form = getattr(self, 'vote_form', forms.Vote(poll=self.object.container.poll))
         vote_forms = {f.instance.option: f for f in vote_form.votes.forms}
-
         kwargs['vote_form'] = vote_form
         kwargs['vote_forms'] = vote_forms
-        kwargs['vote_count'] = vote_count
-        kwargs['winner'] = self.get_winner(vote_count)
+
         return super().get_context_data(**kwargs)
+
+    def get_condorcet_votes(self, votes):
+        return {}
+        
+    def get_simple_votes(self, votes):
+        def get_winner(vote_count: dict):
+            result = []
+            for option, votes in vote_count.items():
+                result.append(
+                    (option, votes['yes'] + votes['maybe'] * .33334)
+                )
+            try:
+                return sorted(result, key=lambda item: item[1], reverse=True)[0][0]
+            except IndexError:
+                return None
+
+        def vote_key(endorse):
+            if endorse is None:
+                return 'maybe'
+            elif endorse:
+                return 'yes'
+            else:
+                return 'no'
+
+        votes_dict = collections.defaultdict(dict)
+        vote_count = collections.defaultdict(lambda: dict(yes=0, no=0, maybe=0))
+        for vote in votes:
+            vote_count[vote.option][vote_key(vote.simplevote.endorse)] += 1
+            if vote.voter:
+                votes_dict[vote.voter][vote.option] = vote
+                votes_dict[vote.voter]['latest'] = vote
+            else:
+                votes_dict[vote.anonymous][vote.option] = vote
+                votes_dict[vote.anonymous]['latest'] = vote
+        print(votes_dict)
+        return {
+                'votes': votes_dict,
+                'vote_count': vote_count,
+                'winner': get_winner(vote_count)
+                }
 
 
 class Vote(core.views.PermissionMixin, django.views.generic.CreateView):
