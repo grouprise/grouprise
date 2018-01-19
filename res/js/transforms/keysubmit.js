1f7161e59dd384f40b2b91dcc71c33d53de717c0
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2018-01-19 19:35:50 +0100

    change submit key combination to ctrl + return. refs #496

--- res/js/transforms/keysubmit.js
+++ res/js/transforms/keysubmit.js
@@ -5,7 +5,7 @@ import { insertElement, remove, keyPressed, on } from 'luett'
 function usage () {
   return (
     bel`<span class='pull-right media-instruction'>
-        <kbd>Shift</kbd> + <kbd>Enter</kbd> sendet das Formular ab
+        <kbd><kbd>Strg</kbd> + <kbd>Enter</kbd></kbd> schickt Deine Nachricht ab
     </span>`
   )
 }
@@ -13,7 +13,7 @@ function usage () {
 export default el => {
   const instructions = insertElement.after(el, usage())
 
-  const keyDownListener = on(el, 'keydown', keyPressed(13, { shiftKey: true }, event => {
+  const keyDownListener = on(el, 'keydown', keyPressed(13, { ctrlKey: true }, event => {
     event.preventDefault()
     closest(el, 'form').dispatchEvent(new window.Event('submit', { bubbles: true, cancelable: true }))
   }))
