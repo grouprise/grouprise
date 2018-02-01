190c13c25226143243f6c84f061d02aa17873cf7
Robert <gestalt@systemausfall.org>
2018-02-01 16:29:36 +0100

    Proper search result display

--- features/associations/apps.py
+++ features/associations/apps.py
@@ -5,4 +5,4 @@ class AssociationsConfig(AppConfig):
     name = "features.associations"
 
     def ready(self):
-        watson.register(self.get_model("Association"), fields=('content__title',))
+        watson.register(self.get_model("Association").objects.exclude_deleted().filter(public=True), fields=('content__title', 'content__place', 'content__versions__text'))
