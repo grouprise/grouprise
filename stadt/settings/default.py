2699b05bd41776948a053922ce6e893d366bf751
Robert <gestalt@systemausfall.org>
2018-02-06 09:03:51 +0100

    Latest state

--- stadt/settings/default.py
+++ stadt/settings/default.py
@@ -62,7 +62,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'watson.middleware.SearchContextMiddleware',
     'features.gestalten.middleware.GestaltAppConfigMiddleware',
     'core.assets.CSPMiddleware',
 ]
