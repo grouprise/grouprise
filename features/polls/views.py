5993440f83c7f4805cc46e53a22659885992cb19
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2018-02-20 04:38:13 +0100

    fix broken django2+ vs django<2 merge

--- features/polls/views.py
+++ features/polls/views.py
@@ -54,7 +54,7 @@ class Vote(core.views.PermissionMixin, django.views.generic.CreateView):
 
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
-        if self.request.user.is_authenticated():
+        if self.request.user.is_authenticated:
             kwargs['instance'] = models.SimpleVote(voter=self.request.user.gestalt)
         kwargs['poll'] = self.association.container.poll
         return kwargs
