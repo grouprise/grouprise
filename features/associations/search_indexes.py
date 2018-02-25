123d57fe56fc789a3bf627707ac98b58ac926163
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2018-02-25 19:39:15 +0100

    remove terms from search text that are longer than 240 chars. refs #216

--- features/associations/search_indexes.py
+++ features/associations/search_indexes.py
@@ -1,11 +1,20 @@
+import re
 from haystack import indexes
 from haystack.indexes import Indexable, SearchIndex
 
 from .models import Association
 
+MAX_TERM_LENGTH_REGEX = re.compile(r'[^\s]{240,}')
+
 
 class AssociationIndex(Indexable, SearchIndex):
     text = indexes.CharField(document=True, use_template=True)
+
+    def prepare_text(self, obj):
+        # xapian doesn’t support terms longer than 245 characters, but
+        # our texts sometimes contain links and other stuff that may be
+        # longer than that. replace everything that is longer than 240 chars
+        return re.sub(MAX_TERM_LENGTH_REGEX, '', self.prepared_data['text'])
 
     def get_model(self):
         return Association
