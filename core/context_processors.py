8ca7b339cf5ee6e439009cc33fc8866bd8021105
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2018-02-26 22:53:39 +0100

    add claim support. refs #495

--- core/context_processors.py
+++ core/context_processors.py
@@ -4,9 +4,10 @@ from django.contrib.sites import shortcuts
 
 def settings(request):
     return {
-            'STADTGESTALTEN_LOGO_URL': django.conf.settings.STADTGESTALTEN_LOGO_URL,
-            'STADTGESTALTEN_SHOW_HEADER': django.conf.settings.STADTGESTALTEN_SHOW_HEADER,
-            }
+        'STADTGESTALTEN_LOGO_URL': django.conf.settings.STADTGESTALTEN_LOGO_URL,
+        'STADTGESTALTEN_SHOW_HEADER': django.conf.settings.STADTGESTALTEN_SHOW_HEADER,
+        'STADTGESTALTEN_CLAIMS': getattr(django.conf.settings, 'STADTGESTALTEN_CLAIMS', [])
+    }
 
 
 def site(request):
