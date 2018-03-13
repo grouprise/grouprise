fbb469d47375c9fba2b4a0a63942e8c7d2c7f560
Robert <gestalt@systemausfall.org>
2018-03-13 14:59:39 +0100

    Added test

--- features/contributions/tests.py
+++ features/contributions/tests.py
@@ -1,13 +1,17 @@
-import django
 from django.core import mail
+from django.test import TestCase
 from django.urls import reverse
 from django_mailbox import models as mailbox_models, signals as mailbox_signals
 
 import features.articles.tests
 from core import tests
 from features.associations import models as associations
+from features.associations.models import Association
+from features.contributions.models import Contribution
+from features.conversations.models import Conversation
 from features.gestalten import tests as gestalten
 from features.memberships import test_mixins as memberships
+from features.memberships.test_mixins import AuthenticatedMemberMixin
 from . import models
 
 
@@ -23,8 +27,7 @@ class ContributionMixin(features.articles.tests.ArticleMixin):
         self.contribution = self.create_contribution()
 
 
-class ContentReplyByEmail(
-        memberships.AuthenticatedMemberMixin, tests.Test):
+class ContentReplyByEmail(AuthenticatedMemberMixin, tests.Test):
     def test_content_reply_by_email(self):
         # create article
         self.client.post(
@@ -87,7 +90,7 @@ class ConversationReplyByEmail(
                 text__text='Text B')
 
 
-class Delete(ContributionMixin, django.test.TestCase):
+class Delete(ContributionMixin, TestCase):
     def test_delete_contribution(self):
         delete_url = reverse(
                 'delete-contribution',
@@ -114,3 +117,28 @@ class Delete(ContributionMixin, django.test.TestCase):
         # article contains comment
         r = self.client.get(article_url)
         self.assertNotContains(r, 'Test Comment')
+
+
+class ReplyToAuthor(AuthenticatedMemberMixin, TestCase):
+    def test_reply_to_author(self):
+        r = self.client.post(
+                reverse('create-group-conversation', args=[self.group.pk]),
+                {'subject': 'Test', 'text': 'Test'}, follow=True)
+        association = Association.objects.last()
+        contribution = Contribution.objects.last()
+        reply_url = reverse('reply-to-author', args=[association.pk, contribution.pk])
+
+        # conversation page contains reply link
+        self.assertContains(r, 'href="{}"'.format(reply_url))
+
+        # member email contains reply link
+        self.assertIn(reply_url, mail.outbox[0].body)
+
+        # reply form renders without errors
+        r = self.client.get(reply_url)
+        self.assertEqual(r.status_code, 200)
+
+        # posting reply generates new conversation
+        r = self.client.post(reply_url, {'subject': 'Reply', 'text': 'Test'})
+        association = Conversation.objects.get(subject='Reply').associations.first()
+        self.assertRedirects(r, reverse('conversation', args=[association.pk]))
