82e2ea992aeedadbcac60c742ebb139deb9043c5
Robert <gestalt@systemausfall.org>
2018-03-13 10:39:43 +0100

    Added reply to author dropdown link to contributions

--- features/contributions/rules.py
+++ features/contributions/rules.py
@@ -1,5 +1,15 @@
 from rules import add_perm, is_authenticated, predicate
 
+from features.conversations.rules import can_view
+
+
+@predicate
+def can_access(user, contribution):
+    for association in contribution.container.associations.all():
+        if can_view(user, association):
+            return True
+    return False
+
 
 @predicate
 def is_conversation(user, contribution):
@@ -11,4 +21,12 @@ def is_creator(user, contribution):
     return contribution.author == user.gestalt
 
 
+@predicate
+def is_multi_user_contribution(user, contribution):
+    associations = contribution.container.associations
+    return associations.count() > 1 or associations.first().entity.is_group
+
+
 add_perm('contributions.delete', ~is_conversation & is_authenticated & is_creator)
+
+add_perm('contributions.reply_to_author', is_multi_user_contribution & can_access)
