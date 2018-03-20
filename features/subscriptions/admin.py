3bb5e6bbb348582ad7ed6ed2878021681345a344
Robert <gestalt@systemausfall.org>
2018-03-20 13:02:52 +0100

    Added subscriptions to admin

---
+++ features/subscriptions/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from . import models
+
+
+class SubscriptionAdmin(admin.ModelAdmin):
+    search_fields = ['subscriber__user__username']
+
+admin.site.register(models.Subscription, SubscriptionAdmin)
