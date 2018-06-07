ff29796e4b3ad093262dcf22db2b60692628dcb5
Robert <aldrin+gitlab@systemausfall.org>
2018-06-07 16:13:48 +0200

    Add resign urls to notifications

--- features/memberships/urls.py
+++ features/memberships/urls.py
@@ -20,8 +20,18 @@ urlpatterns = [
     urls.url(
         r'^groups/(?P<group_pk>[0-9]+)/resign/$',
         views.Resign.as_view(),
-        name='resign'),
-
+        name='resign'
+    ),
+    urls.url(
+        r'^groups/resign/confirm/(?P<secret_key>[a-z0-9]+)/$',
+        views.ResignConfirm.as_view(),
+        name='resign-confirm'
+    ),
+    urls.url(
+        r'^groups/(?P<group_pk>[0-9]+)/resign/request/$',
+        views.ResignRequest.as_view(),
+        name='resign-request'
+    ),
     urls.url(
         r'^associations/(?P<association_pk>[0-9]+)/apply/$',
         views.Apply.as_view(),
