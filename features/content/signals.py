761cd58b6d5469653f3c46bb81db978f732dae9e
Robert <aldrin+gitlab@systemausfall.org>
2018-11-08 09:54:22 +0100

    Send content notification only if content is less than 1 week old, refs #507

--- features/content/signals.py
+++ features/content/signals.py
@@ -1,4 +1,7 @@
+from datetime import timedelta
+
 from django.dispatch import receiver, Signal
+from django.utils.timezone import now
 
 from . import notifications
 
@@ -7,4 +10,5 @@ post_create = Signal(providing_args=['instance'])
 
 @receiver(post_create)
 def content_created(sender, instance, **kwargs):
-    notifications.ContentCreated.send_all(instance)
+    if now() - instance.versions.last().time_created < timedelta(weeks=1):
+        notifications.ContentCreated.send_all(instance)
