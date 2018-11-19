a7566d528eaabe21d58037c7fbf7d68a2b043dcc
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2018-11-19 12:13:23 +0100

    implement organisation endpoint for transition connect API
    Adds the organisation endpoint from the transition connect API draft.
    Only the permalink field is a custom extension representing an absolute
    url to the group page.

--- features/groups/rest_api.py
+++ features/groups/rest_api.py
@@ -1,6 +1,5 @@
 from rest_framework import viewsets, serializers, permissions
 from rest_framework.decorators import permission_classes
-import django_filters
 import django_filters.widgets
 
 from . import models
@@ -26,8 +25,8 @@ class GroupSerializer(serializers.ModelSerializer):
     url = serializers.CharField(source='get_absolute_url', read_only=True)
     cover = serializers.SerializerMethodField()
 
-    def get_cover(self, instance: models.Group):
-        return instance.get_cover_url()
+    def get_cover(self, obj: models.Group):
+        return obj.get_cover_url()
 
     class Meta:
         model = models.Group
@@ -43,6 +42,72 @@ class GroupSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.Group.objects.all()
 
 
+class TransitionConnectGroupListSerializer(serializers.ModelSerializer):
+    id = serializers.CharField(read_only=True)
+    permalink = serializers.SerializerMethodField()
+    timestamp = serializers.SerializerMethodField()
+
+    def get_permalink(self, obj: models.Group):
+        return self.context['request'].build_absolute_uri(obj.get_absolute_url())
+
+    def get_timestamp(self, obj: models.Group):
+        return int(obj.time_modified.timestamp())
+
+    class Meta:
+        model = models.Group
+        fields = ('id', 'permalink', 'timestamp')
+
+
+class TransitionConnectGroupSerializer(TransitionConnectGroupListSerializer):
+    website = serializers.CharField(source='url', read_only=True)
+    categories = serializers.SerializerMethodField()
+    admins = serializers.SerializerMethodField()
+    slogan = serializers.SerializerMethodField()
+    locations = serializers.SerializerMethodField()
+
+    def get_categories(self, obj: models.Group):
+        # TODO: we don’t have categories for groups yet, only tags. should we deliver those?
+        return []
+
+    def get_admins(self, obj: models.Group):
+        # TODO: should we consider members admins and if so, are we happy to provide
+        #       their mail-address to the outside world?
+        return []
+
+    def get_slogan(self, obj: models.Group):
+        # TODO: groups don’t have slogans
+        return None
+
+    def get_locations(self, obj: models.Group):
+        # TODO: groups only have a single address, but we don’t support a description
+        #       or any kind of geo-data for them
+        address = ', '.join(obj.address.replace('\r', '').split('\n')) \
+            if obj.address else None
+        return [{
+            'address': address,
+            'description': None,
+            'geo': None
+        }]
+
+    class Meta:
+        model = models.Group
+        fields = (
+            'id', 'permalink', 'timestamp', 'name', 'description', 'admins',
+            'categories', 'website', 'slogan', 'locations'
+        )
+
+
+@permission_classes((permissions.AllowAny, ))
+class TransitionGroupSet(viewsets.ReadOnlyModelViewSet):
+    queryset = models.Group.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return TransitionConnectGroupListSerializer
+        return TransitionConnectGroupSerializer
+
+
 @api.register
 def load(router):
     router.register(r'groups', GroupSet, 'group')
+    router.register(r'organisation', TransitionGroupSet, 'organisation')
