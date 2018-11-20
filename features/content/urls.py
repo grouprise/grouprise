c799e8efc9c28f44d3ee80342e82bac5742111c0
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2018-11-20 23:31:58 +0100

    fix content metadata. refs #395
    OpenGraph clients could not properly parse our content because
    `content.get_absolute_url` didn’t return anything and the canonical url
    of our content therefore always referred to `http_origin`. This
    basically broke everything as only the canonical URL is used to create
    the OpenGraph dataset.
    
    Images were broken as well, as the way the image was stored was changed
    in the backend without updating this template. We now have dedicated
    Facebook and Twitter meta image fields.

--- features/content/urls.py
+++ features/content/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
         views.Detail.as_view(),
         name='content'),
 
+    # This route is used in the content/_meta.html template. If there is a change in how
+    # generated urls for this route work the meta urls should be checked afterwards.
     urls.url(
         r'^stadt/content/(?P<association_pk>[0-9]+)/$',
         views.Permalink.as_view(),
