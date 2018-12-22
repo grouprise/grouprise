7cccd84b2e918e4a63bf7ef075864790964ef77d
Lars Kruse <devel@sumpfralle.de>
2018-12-22 20:57:54 +0100

    Makefile: clarify usage of "STATIC_FONT*" variables
    Previously it was not obvious, that these variables refer to
    directories.

--- make.d/assets.mk
+++ make.d/assets.mk
@@ -16,8 +16,8 @@ DEPS_WEBPACK = $(shell find "webpack" -type f)
 BUILD_FONT_GOOGLE = $(DIR_BUILD)/fonts/google
 
 # static output files
-STATIC_FONT = $(DIR_BUILD)/static/fonts
-STATIC_FONT_GOOGLE = $(STATIC_FONT)/google
+DIR_STATIC_FONT = $(DIR_BUILD)/static/fonts
+DIR_STATIC_FONT_GOOGLE = $(DIR_STATIC_FONT)/google
 
 URL_FONT_GOOGLE = https://fonts.googleapis.com/css?family=Roboto+Slab:300,400,700|Roboto:300,400,400i,500,700
 
@@ -28,24 +28,24 @@ $(DIR_NODE): $(BIN_NODE) package.json
 	env -i $(RUN_NODE) "$(BIN_NODE_PKG)" ci --no-progress
 	touch -c "$(DIR_NODE)"
 
-$(STATIC_FONT_GOOGLE): $(DIR_NODE)
+$(DIR_STATIC_FONT_GOOGLE): $(DIR_NODE)
 	mkdir -p "$(BUILD_FONT_GOOGLE)"
 	$(RUN_NODE) "$(BIN_FONTDUMP)" --target-directory "$(BUILD_FONT_GOOGLE)" --web-directory "." "$(URL_FONT_GOOGLE)"
-	mkdir -p "$(STATIC_FONT_GOOGLE)"
-	cp -a --target-directory "$(STATIC_FONT_GOOGLE)" \
+	mkdir -p "$(DIR_STATIC_FONT_GOOGLE)"
+	cp -a --target-directory "$(DIR_STATIC_FONT_GOOGLE)" \
 		"$(BUILD_FONT_GOOGLE)"/*.eot \
 		"$(BUILD_FONT_GOOGLE)"/*.svg \
 		"$(BUILD_FONT_GOOGLE)"/*.ttf \
 		"$(BUILD_FONT_GOOGLE)"/*.woff \
 		"$(BUILD_FONT_GOOGLE)"/*.woff2
-	touch "$(STATIC_FONT_GOOGLE)"
+	touch "$(DIR_STATIC_FONT_GOOGLE)"
 
-$(DIR_STATIC): $(STATIC_FONT_GOOGLE) $(DEPS_ASSETS) $(DEPS_WEBPACK)
+$(DIR_STATIC): $(DIR_STATIC_FONT_GOOGLE) $(DEPS_ASSETS) $(DEPS_WEBPACK)
 	NODE_ENV=production $(RUN_NODE) $(BIN_WEBPACK) --bail
 	touch "$(DIR_STATIC)"
 
 .PHONY: assets_fonts
-assets_fonts: $(STATIC_FONT_GOOGLE)
+assets_fonts: $(DIR_STATIC_FONT_GOOGLE)
 
 .PHONY: assets_webpack
 assets_webpack: $(DIR_STATIC)
