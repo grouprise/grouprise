c6a57a5a42ca4fa117d08ef1af43d7de315c0d11
Lars Kruse <devel@sumpfralle.de>
2018-12-22 21:16:10 +0100

    Makefile: use stamp file for google fonts download
    Previously a failed download would have created the directory and thus
    marked the the target as updated.

--- make.d/assets.mk
+++ make.d/assets.mk
@@ -18,6 +18,7 @@ BUILD_FONT_GOOGLE = $(DIR_BUILD)/fonts/google
 # static output files
 DIR_STATIC_FONT = $(DIR_BUILD)/static/fonts
 DIR_STATIC_FONT_GOOGLE = $(DIR_STATIC_FONT)/google
+STAMP_STATIC_FONT_GOOGLE = $(DIR_BUILD)/.static_font_google
 
 URL_FONT_GOOGLE = https://fonts.googleapis.com/css?family=Roboto+Slab:300,400,700|Roboto:300,400,400i,500,700
 
@@ -28,7 +29,7 @@ $(DIR_NODE): $(BIN_NODE) package.json
 	env -i $(RUN_NODE) "$(BIN_NODE_PKG)" ci --no-progress
 	touch -c "$(DIR_NODE)"
 
-$(DIR_STATIC_FONT_GOOGLE): $(DIR_NODE)
+$(STAMP_STATIC_FONT_GOOGLE): $(DIR_NODE)
 	mkdir -p "$(BUILD_FONT_GOOGLE)"
 	$(RUN_NODE) "$(BIN_FONTDUMP)" --target-directory "$(BUILD_FONT_GOOGLE)" --web-directory "." "$(URL_FONT_GOOGLE)"
 	mkdir -p "$(DIR_STATIC_FONT_GOOGLE)"
@@ -38,14 +39,14 @@ $(DIR_STATIC_FONT_GOOGLE): $(DIR_NODE)
 		"$(BUILD_FONT_GOOGLE)"/*.ttf \
 		"$(BUILD_FONT_GOOGLE)"/*.woff \
 		"$(BUILD_FONT_GOOGLE)"/*.woff2
-	touch "$(DIR_STATIC_FONT_GOOGLE)"
+	touch "$(STAMP_STATIC_FONT_GOOGLE)"
 
-$(DIR_STATIC): $(DIR_STATIC_FONT_GOOGLE) $(DEPS_ASSETS) $(DEPS_WEBPACK)
+$(DIR_STATIC): $(STAMP_STATIC_FONT_GOOGLE) $(DEPS_ASSETS) $(DEPS_WEBPACK)
 	NODE_ENV=production $(RUN_NODE) $(BIN_WEBPACK) --bail
 	touch "$(DIR_STATIC)"
 
 .PHONY: assets_fonts
-assets_fonts: $(DIR_STATIC_FONT_GOOGLE)
+assets_fonts: $(STAMP_STATIC_FONT_GOOGLE)
 
 .PHONY: assets_webpack
 assets_webpack: $(DIR_STATIC)
