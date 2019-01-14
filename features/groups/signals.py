a4e614c77764bd84d97c7e960a68072efc9cc8e5
Robert <aldrin+gitlab@systemausfall.org>
2019-01-14 13:37:45 +0100

    Fix usage of ENTITY_SLUG_BLACKLIST

--- features/groups/signals.py
+++ features/groups/signals.py
@@ -1,9 +1,9 @@
-import django
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 import core
 from features.gestalten import models as gestalten
+from features.stadt.forms import ENTITY_SLUG_BLACKLIST
 from . import models
 
 
@@ -12,7 +12,7 @@ def post_group_save(sender, instance, created, **kwargs):
     if created:
         instance.slug = core.models.get_unique_slug(
                 models.Group, {'slug': core.text.slugify(instance.name)},
-                reserved_slugs=django.conf.settings.ENTITY_SLUG_BLACKLIST,
+                reserved_slugs=ENTITY_SLUG_BLACKLIST,
                 reserved_slug_qs=gestalten.Gestalt.objects,
                 reserved_slug_qs_field='user__username')
         instance.save()
