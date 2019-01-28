8f857b2a6d1afbadda293ba725e5cd636383fe27
Robert <aldrin+gitlab@systemausfall.org>
2019-01-28 17:10:08 +0100

    Extract autodiscovery to module init

--- core/apps.py
+++ core/apps.py
@@ -5,7 +5,4 @@ class CoreConfig(AppConfig):
     name = 'core'
 
     def ready(self):
-        from django.utils.module_loading import autodiscover_modules
-
-        for module_name in ['rules', 'signals']:
-            autodiscover_modules(module_name)
+        self.module.autodiscover()
