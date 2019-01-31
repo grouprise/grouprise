c56031f5049b5cbc742e34f3876e56c7aa7fe3b7
Robert <aldrin+gitlab@systemausfall.org>
2019-01-31 08:38:16 +0100

    Rename claims cp, remove http_origin

--- core/context_processors.py
+++ core/context_processors.py
@@ -4,12 +4,11 @@ from django.contrib.sites import shortcuts
 
 def settings(request):
     return {
-        'STADTGESTALTEN_CLAIMS': django_settings.GROUPRISE.get('CLAIMS', [])
+        'GROUPRISE_CLAIMS': django_settings.GROUPRISE.get('CLAIMS', [])
     }
 
 
 def site(request):
     return {
-        'http_origin': request.build_absolute_uri("/").rstrip("/"),
         'site': shortcuts.get_current_site(request),
     }
