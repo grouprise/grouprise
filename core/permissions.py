123765c1292d5c5c132b5b88d119e82e4dc4c280
Robert <aldrin+gitlab@systemausfall.org>
2019-03-14 15:03:00 +0100

    Document core.permissions

--- core/permissions.py
+++ core/permissions.py
@@ -2,6 +2,11 @@ from rest_framework import permissions
 
 
 class RulesPermissions(permissions.BasePermission):
+    '''Default permission class used with Django Rest Framework
+
+    .. seealso:: https://www.django-rest-framework.org/api-guide/permissions/
+    '''
+
     def has_permission(self, request, view):
         try:
             return view.has_permission()
