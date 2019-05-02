04a4b9a5a144d1ba7431ed7d0a806d453f02daa5
Robert <aldrin@systemausfall.org>
2019-05-02 14:16:21 +0200

    Remove core.signals

--- features/memberships/signals.py
+++ features/memberships/signals.py
@@ -1,21 +1,23 @@
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from core import signals
 from features.groups import models as groups
 from . import models, notifications
 
 
-@receiver(signals.model_created, sender=groups.Group)
-def add_first_member(sender, instance, **kwargs):
-    group = instance
-    if group.gestalt_created:
-        models.Membership.objects.create(
-                created_by=group.gestalt_created,
-                group=group, member=group.gestalt_created)
+@receiver(post_save, sender=groups.Group)
+def add_first_member(sender, instance, created, **kwargs):
+    if created:
+        group = instance
+        if group.gestalt_created:
+            models.Membership.objects.create(
+                    created_by=group.gestalt_created,
+                    group=group, member=group.gestalt_created)
 
 
-@receiver(signals.model_created, sender=models.Membership)
-def send_membership_notification(sender, instance, **kwargs):
-    membership = instance
-    if membership.created_by != membership.member:
-        notifications.MembershipCreated(membership).send(membership.member)
+@receiver(post_save, sender=models.Membership)
+def send_membership_notification(sender, instance, created, **kwargs):
+    if created:
+        membership = instance
+        if membership.created_by != membership.member:
+            notifications.MembershipCreated(membership).send(membership.member)
