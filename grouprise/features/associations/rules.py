e19ca1afad4b4cfa079b64c53f39c71fb6f8d5db
Robert <aldrin@systemausfall.org>
2019-06-20 15:57:52 +0200

    Allow deleting any group content if membership is older than one week, refs #641

--- grouprise/features/associations/rules.py
+++ grouprise/features/associations/rules.py
@@ -1,4 +1,7 @@
+from datetime import date, timedelta
 from rules import add_perm, is_authenticated, predicate
+
+from grouprise.features.memberships.models import Membership
 
 
 @predicate
@@ -6,4 +9,16 @@ def is_creator(user, association):
     return association.container.versions.first().author == user.gestalt
 
 
-add_perm('associations.delete', is_authenticated & is_creator)
+@predicate
+def is_long_standing_group_member(user, association):
+    if not association.entity.is_group:
+        return False
+    membership = Membership.objects.filter(group=association.entity, member=user.gestalt) \
+                                   .first()
+    if not membership:
+        return False
+    return membership.date_joined < date.today() - timedelta(weeks=1)
+
+
+add_perm('associations.delete', is_authenticated \
+                                & (is_creator | is_long_standing_group_member))
