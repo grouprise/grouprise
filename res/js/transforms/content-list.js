171833b049a3511efe8db53c9bc9e394722d2aeb
Robert <aldrin@systemausfall.org>
2019-06-20 10:20:03 +0200

    Add basic content list vue component

---
+++ res/js/transforms/content-list.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import ContentList from '../components/content/content-list.vue'
+
+export default (el, opts) => {
+  const state = {}
+
+  const vue = new Vue({
+    el,
+    data: state,
+    render (h) {
+      return h(ContentList, {
+        props: {}
+      })
+    },
+  })
+
+  return {
+    remove () {
+      vue.$destroy()
+    }
+  }
+}
