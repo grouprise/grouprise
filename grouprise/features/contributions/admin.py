ee6d0a03134204ec878b8a1c445a5409273406fa
Robert <aldrin@systemausfall.org>
2019-08-08 12:10:27 +0200

    Show internal contributions in admin, refs #659

--- grouprise/features/contributions/admin.py
+++ grouprise/features/contributions/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register(models.Contribution)
+
+class ContributionModelAdmin(admin.ModelAdmin):
+
+    def get_queryset(self, request):
+        qs = self.model.objects_with_internal.get_queryset()
+        ordering = self.get_ordering(request)
+        if ordering:
+            qs = qs.order_by(*ordering)
+        return qs
+
+
+admin.site.register(models.Contribution, ContributionModelAdmin)
 admin.site.register(models.Text)
