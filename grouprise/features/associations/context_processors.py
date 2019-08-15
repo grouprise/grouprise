7cc5a6f22ee5569e25bd6e960016b6630e89d051
Robert <aldrin@systemausfall.org>
2019-08-15 11:34:27 +0200

    Use count() instead of len(), refs #607

--- grouprise/features/associations/context_processors.py
+++ grouprise/features/associations/context_processors.py
@@ -4,5 +4,5 @@ from grouprise.features.associations.models import Association
 def activity(request):
     num_activity = None
     if request.user.is_authenticated:
-        num_activity = len(Association.objects.active_ordered_user_associations(request.user))
+        num_activity = Association.objects.active_ordered_user_associations(request.user).count()
     return {'num_activity': num_activity}
