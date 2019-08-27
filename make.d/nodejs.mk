df51539cb6b46b15e15e079216713a0d161b1377
Lars Kruse <devel@sumpfralle.de>
2019-08-27 02:42:31 +0200

    Makefile: rename "NODE_DEST" to "NODE_LOCAL_DIR"
    This should describe the purpose of this variable a bit better.

--- make.d/nodejs.mk
+++ make.d/nodejs.mk
@@ -7,24 +7,24 @@ NODE_VERSION_MIN = v8.12.0
 NODE_URL_X86 = https://nodejs.org/dist/$(NODE_VERSION_MIN)/node-$(NODE_VERSION_MIN)-linux-x86.tar.xz
 NODE_URL_X64 = https://nodejs.org/dist/$(NODE_VERSION_MIN)/node-$(NODE_VERSION_MIN)-linux-x64.tar.xz
 NODE_URL = $(shell [ "$$(uname -m)" = "x86_64" ] && echo "$(NODE_URL_X64)" || echo "$(NODE_URL_X86)")
-NODE_DEST = $(DIR_BUILD)/node
+NODE_LOCAL_DIR = $(DIR_BUILD)/node
 
 RUN_NODE = PATH="$$PATH:$$(dirname "$(BIN_NODE)")" DIR_BUILD="$(abspath $(DIR_BUILD))" node
 
 
 $(BIN_NODE):
-	rm -rf "$(NODE_DEST)"
-	mkdir -p "$(NODE_DEST)/bin"
+	rm -rf "$(NODE_LOCAL_DIR)"
+	mkdir -p "$(NODE_LOCAL_DIR)/bin"
 	@if [  "$(NODE_VERSION)" = $$(printf '%s\n' "$(NODE_VERSION)" "$(NODE_VERSION_MIN)" | sort -V | head -n1) ] || ! hash npm 2>/dev/null; then \
 		if [ "$$(uname -s)" != "Linux" ]; then \
 			echo >&2 "NodeJS download for non-linux platforms is sadly not supported. Please install $(NODE_VERSION_MIN) or later manually."; \
 			exit 1; fi; \
 		echo >&2 "Local nodejs version is too old (before $(NODE_VERSION_MIN)). Downloading from server ..."; \
-		wget -O - "$(NODE_URL)" | tar -xJ -C "$(NODE_DEST)" --strip-components=1 -f -; \
+		wget -O - "$(NODE_URL)" | tar -xJ -C "$(NODE_LOCAL_DIR)" --strip-components=1 -f -; \
 	else \
 		echo >&2 "Local nodejs version is sufficient. Symlinking executables ..."; \
-		ln -s "$(BIN_NODE_SYSTEM)" "$(NODE_DEST)/bin/node"; \
-		ln -s "$$(which npm)" "$(NODE_DEST)/bin/npm"; \
+		ln -s "$(BIN_NODE_SYSTEM)" "$(NODE_LOCAL_DIR)/bin/node"; \
+		ln -s "$$(which npm)" "$(NODE_LOCAL_DIR)/bin/npm"; \
 	fi
 
 $(BIN_NODE_PKG): $(BIN_NODE)
