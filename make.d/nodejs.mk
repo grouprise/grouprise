grouprise/grouprise

commit be8f81a5285c2bf82ada9f0ab96826b6bf64c702

Makefile: introduce "BIN_NPM_SYSTEM"
Now BIN_NODE and BIN_NPM (as well as their "*_SYSTEM" counterparts) are
used in the same way.

make.d/nodejs.mk
@@ -1,4 +1,5 @@
 BIN_NODE_SYSTEM = $(shell which nodejs node || true)
+BIN_NPM_SYSTEM = $(shell which npm || true)
 BIN_NODE ?= $(DIR_BUILD)/node/bin/node
 BIN_NPM ?= $(DIR_BUILD)/node/bin/npm
 
@@ -24,7 +25,7 @@ $(BIN_NODE):
 	else \
 		echo >&2 "Local nodejs version is sufficient. Symlinking executables ..."; \
 		ln -s "$(BIN_NODE_SYSTEM)" "$(NODE_LOCAL_DIR)/bin/node"; \
-		ln -s "$$(which npm)" "$(NODE_LOCAL_DIR)/bin/npm"; \
+		ln -s "$(BIN_NPM_SYSTEM)" "$(NODE_LOCAL_DIR)/bin/npm"; \
 	fi
 
 $(BIN_NPM): $(BIN_NODE)

commit 78ec00acf8fd35e7b7624ba95af6d7fc18716f0b

make.d/nodejs: handle systems with "nodejs" and "node" executables
The check for the installed nodejs version was broken with be8f81a52.
The check always failed and thus initiated the download of a local
nodejs installtion.

make.d/nodejs.mk
@@ -1,4 +1,4 @@
-BIN_NODE_SYSTEM = $(shell which nodejs node || true)
+BIN_NODE_SYSTEM = $(shell which nodejs node | head -1)
 BIN_NPM_SYSTEM = $(shell which npm || true)
 NODE_LOCAL_DIR = $(DIR_BUILD)/node
 BIN_NODE ?= $(NODE_LOCAL_DIR)/bin/node