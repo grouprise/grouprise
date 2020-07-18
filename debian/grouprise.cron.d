ee0700cecfe0abb9f31c694136a519d892d26d14
Lars Kruse <devel@sumpfralle.de>
2020-07-18 15:43:43 +0200

    deb: execute cron-jobs as "_grouprise" instead of "root"
    The "grouprisectl" script would anyway switch to the packaged user
    immediately.  Thus we can skip using root privileges.

--- debian/grouprise.cron.d
+++ debian/grouprise.cron.d
@@ -2,17 +2,17 @@
 # The LMTP daemon (since grouprise 2.3) is supposed to handle incoming mail in the future.
 # This requires changes of the SMTP server setup. We will start with single recipient groups, until
 # all traffic is handled by LMTP.
-*/3 * * * * root        chronic sh -c 'grouprisectl getmail 2>&1 | grep -vE "^(INFO:|WARNING:django_mailbox.models:.*content-transfer-encoding)"'
+*/3 * * * * _grouprise        chronic sh -c 'grouprisectl getmail 2>&1 | grep -vE "^(INFO:|WARNING:django_mailbox.models:.*content-transfer-encoding)"'
 
 # calculate scores for groups and users
-23  * * * * root        chronic grouprisectl update_scores
+23  * * * * _grouprise        chronic grouprisectl update_scores
 
 # import content from external websites
-46  * * * * root        chronic grouprisectl import_feeds
+46  * * * * _grouprise        chronic grouprisectl import_feeds
 
 # send mails with django-mailer
-*   * * * * root        cd /tmp && chronic sh -c "grouprisectl send_mail 2>&1 | tee -a /var/log/grouprise/mailer-grouprise.log"
-9,29,49 * * * * root    cd /tmp && chronic sh -c "grouprisectl retry_deferred 2>&1 | tee -a /var/log/grouprise/mailer-grouprise.log"
+*   * * * * _grouprise        cd /tmp && chronic sh -c "grouprisectl send_mail 2>&1 | tee -a /var/log/grouprise/mailer-grouprise.log"
+9,29,49 * * * * _grouprise    cd /tmp && chronic sh -c "grouprisectl retry_deferred 2>&1 | tee -a /var/log/grouprise/mailer-grouprise.log"
 
 # update search index
-*/5 * * * * root        chronic grouprisectl update_index --remove
+*/5 * * * * _grouprise        chronic grouprisectl update_index --remove
