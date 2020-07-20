01728f1ce1f1e1f19614155fae0789fff9239e36
Lars Kruse <devel@sumpfralle.de>
2020-07-20 23:28:28 +0200

    make: ensure the existence of the local configuration file before "virtualenv-check"
    Previously the build worked only on hosts with more than one CPU, since
    "make dist-deb" (respectively "dpkg-buildpackage") was executing the
    debian/rules makefile with two or more jobs in parallel.
    
    Now it should also work on hosts with only a single CPU and without
    parallel execution.

--- make.d/app.mk
+++ make.d/app.mk
@@ -6,15 +6,18 @@ $(CONFIG_APP_SETUP): $(GROUPRISE_MAKEFILES)
 	cp "$(CONFIG_APP_SETUP_TEMPLATE)" "$(CONFIG_APP_SETUP)"
 
 .PHONY: app_migrate
-app_migrate: virtualenv-check app_local_settings
+app_migrate: virtualenv-check
 	( . "$(ACTIVATE_VIRTUALENV)" && "$(PYTHON_BIN)" manage.py migrate )
+
+# makefilet runs "manage.py check" during "virtualenv-check" - thus we need the settings beforehand
+virtualenv-check: app_local_settings
 
 .PHONY: app_run
 app_run: app_migrate app_collect_static
 	( . "$(ACTIVATE_VIRTUALENV)" && STADTGESTALTEN_PRESET=development "$(PYTHON_BIN)" manage.py runserver )
 
 .PHONY: app_collect_static
-app_collect_static: virtualenv-check app_local_settings assets
+app_collect_static: virtualenv-check assets
 	( . "$(ACTIVATE_VIRTUALENV)" && STADTGESTALTEN_PRESET=packaging "$(PYTHON_BIN)" manage.py collectstatic --no-input )
 
 .PHONY: app_local_settings
