085aa906ee634777d2426dac39bb7f900956d239
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2020-08-12 00:56:41 +0200

    replace outdated raven library with sentry-sdk
    Sentry SDK configuration now happens via the django/grouprise settings
    by setting the SENTRY_DSN variable.

--- grouprise/core/apps.py
+++ grouprise/core/apps.py
@@ -1,4 +1,7 @@
 from django.apps import AppConfig
+from django.conf import settings
+
+from grouprise import __release__
 
 
 class CoreConfig(AppConfig):
@@ -6,3 +9,15 @@ class CoreConfig(AppConfig):
 
     def ready(self):
         self.module.autodiscover()
+
+        # configure sentry if DSN has been defined
+        if settings.SENTRY_DSN:
+            import sentry_sdk
+            from sentry_sdk.integrations.django import DjangoIntegration
+            sentry_sdk.init(
+                dsn=settings.SENTRY_DSN,
+                integrations=[DjangoIntegration()],
+                send_default_pii=True,
+                environment=settings.ENVIRONMENT,
+                release=f'grouprise@{__release__}',
+            )
--- grouprise/wsgi.py
+++ grouprise/wsgi.py
@@ -9,16 +9,17 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 
 import os
 
+from django.conf import settings
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "grouprise.settings")
 
 application = get_wsgi_application()
 
-ENABLE_SENTRY = os.environ.get("SENTRY_ENABLE", "false") == "true"
-if ENABLE_SENTRY:
-    from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
-    application = Sentry(application)
+# configure sentry if DSN has been defined
+if settings.SENTRY_DSN:
+    from sentry_sdk.integrations.wsgi import SentryWsgiMiddleware
+    application = SentryWsgiMiddleware(application)
 
 PROFILING_DIR = os.environ.get("PROFILING_DIRECTORY", False)
 if PROFILING_DIR:
