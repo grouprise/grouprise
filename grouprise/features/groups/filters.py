3b0cec6a2a3d26f151975ae87b704b9c58371fc2
Robert <aldrin@systemausfall.org>
2020-09-11 11:44:04 +0200

    Add membership groups filter
    with link to groups menu
    
    refs #673

--- grouprise/features/groups/filters.py
+++ grouprise/features/groups/filters.py
@@ -9,6 +9,13 @@ class Group(filters.FilterSet):
             label='Name', lookup_expr='icontains',
             widget=forms.TextInput(attrs={'placeholder': 'z.B. Foodcoop'}))
 
+    only_with_membership = filters.BooleanFilter(method='filter_by_membership')
+
     class Meta:
         model = models.Group
         fields = ['name']
+
+    def filter_by_membership(self, queryset, name, value):
+        if self.request.user.is_authenticated and value:
+            queryset = queryset.filter(members=self.request.user.gestalt)
+        return queryset
