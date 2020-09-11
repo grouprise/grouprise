b7fee24f75dfd52e76f214a549288682a59706d1
Lars Kruse <devel@sumpfralle.de>
2020-09-11 02:02:29 +0200

    Scores: simplify access to "SCORE_CONTENT_AGE" and improve clarity

--- grouprise/features/stadt/scores.py
+++ grouprise/features/stadt/scores.py
@@ -12,28 +12,31 @@ from grouprise.features.groups import models as groups
 from grouprise.features.subscriptions import models as subscriptions
 
 
+MAX_SCORE_CONTENT_AGE_DAYS = settings.GROUPRISE.get('SCORE_CONTENT_AGE', 0)
+
+
 class Gestalt:
     @classmethod
     def score(cls, instance):
-        THRESHOLD = django.utils.timezone.now() - datetime.timedelta(
-                days=settings.GROUPRISE.get('SCORE_CONTENT_AGE', 0))
+        minimum_timestamp = (
+            django.utils.timezone.now() - datetime.timedelta(days=MAX_SCORE_CONTENT_AGE_DAYS))
         s = 0
         if isinstance(instance, gestalten.Gestalt):
             authored_versions = content.Version.objects.filter(
-                    author=instance, time_created__gt=THRESHOLD)
+                    author=instance, time_created__gt=minimum_timestamp)
             authored_contributions = contributions.Contribution.objects.filter(
-                    author=instance, time_created__gt=THRESHOLD)
+                    author=instance, time_created__gt=minimum_timestamp)
             for qs in (authored_versions, authored_contributions):
                 for authored in qs:
-                    s += (authored.time_created - THRESHOLD).days
+                    s += (authored.time_created - minimum_timestamp).days
         return s
 
 
 class Group:
     @classmethod
     def score(cls, instance):
-        THRESHOLD = django.utils.timezone.now() - datetime.timedelta(
-                days=settings.GROUPRISE.get('SCORE_CONTENT_AGE', 0))
+        minimum_timestamp = (
+            django.utils.timezone.now() - datetime.timedelta(days=MAX_SCORE_CONTENT_AGE_DAYS))
         s = 0
         if isinstance(instance, groups.Group):
             num_members = instance.memberships.count()
@@ -43,11 +46,11 @@ class Group:
                 group_versions = content.Version.objects.filter(
                        content__associations__entity_id=instance.id,
                        content__associations__entity_type=instance.content_type,
-                       time_created__gt=THRESHOLD)
+                       time_created__gt=minimum_timestamp)
                 group_comments = contributions.Contribution.objects.filter(
                        content__associations__entity_id=instance.id,
                        content__associations__entity_type=instance.content_type,
-                       time_created__gt=THRESHOLD)
+                       time_created__gt=minimum_timestamp)
                 if only_public:
                     group_versions = group_versions.filter(content__associations__public=True)
                     group_comments = group_comments.filter(content__associations__public=True)
@@ -56,10 +59,10 @@ class Group:
                     group_conversations = contributions.Contribution.objects.filter(
                            conversation__associations__entity_id=instance.id,
                            conversation__associations__entity_type=instance.content_type,
-                           time_created__gt=THRESHOLD)
+                           time_created__gt=minimum_timestamp)
                 for qs in (group_versions, group_comments, group_conversations):
                     for group_authored in qs:
-                        ps += (group_authored.time_created - THRESHOLD).days
+                        ps += (group_authored.time_created - minimum_timestamp).days
                 if num_gestalten > 0:
                     s += ps * log(num_gestalten) * (10 if only_public else 1)
         return s
