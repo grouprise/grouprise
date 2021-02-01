edc6e453a84b504406fc30965c10c38e745283c5
Lars Kruse <devel@sumpfralle.de>
2021-02-01 17:00:29 +0100

    Search: allow partial matches of words
    Previously the search term "gestalten" would not retrieve the
    "stadtgestalten" group.

--- grouprise/features/associations/search_indexes.py
+++ grouprise/features/associations/search_indexes.py
@@ -1,6 +1,6 @@
 import re
 from haystack import indexes
-from haystack.indexes import Indexable, SearchIndex
+from haystack.indexes import Indexable, NgramField, SearchIndex
 
 from .models import Association
 
@@ -9,6 +9,9 @@ MAX_TERM_LENGTH_REGEX = re.compile(r'[^\s]{240,}')
 
 class AssociationIndex(Indexable, SearchIndex):
     text = indexes.CharField(document=True, use_template=True)
+    # the n-gram analyzer allows partial matches anywhere in the words
+    # (EdgeNgramField would match only at the beginning)
+    slug = NgramField(model_attr='slug')
 
     def prepare_text(self, obj):
         # xapian doesn’t support terms longer than 245 characters, but
--- grouprise/features/groups/search_indexes.py
+++ grouprise/features/groups/search_indexes.py
@@ -1,11 +1,17 @@
 import haystack
-from haystack.indexes import Indexable, SearchIndex
+from haystack.indexes import Indexable, NgramField, SearchIndex
 
 from .models import Group
 
 
 class GroupIndex(Indexable, SearchIndex):
     text = haystack.indexes.CharField(document=True, use_template=True)
+    # the n-gram analyzer allows partial matches anywhere in the words
+    # (EdgeNgramField would match only at the beginning)
+    address = NgramField(model_attr="address")
+    description = NgramField(model_attr="description")
+    name = NgramField(model_attr="name")
+    slug = NgramField(model_attr="slug")
 
     def get_model(self):
         return Group
