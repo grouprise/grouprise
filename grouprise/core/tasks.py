85243adc5d6b7a07b5f8cc243131813de3e1308e
Lars Kruse <devel@sumpfralle.de>
2021-02-01 20:36:41 +0100

    auto_task: rename variables and document purpose of the function
    The variable names were a bit misleading (model/method instead of
    object/function) and the purpose of the function was not obvious.

--- grouprise/core/tasks.py
+++ grouprise/core/tasks.py
@@ -6,13 +6,26 @@ from grouprise.core.models import RepeatableTask
 
 
 @db_task()
-def auto_task(model):
+def auto_task(obj):
+    """Store an object-related task temporarily in the database, execute it and remove the db entry
+
+    The task is removed from the database after execution, if it finished successfully.
+    In case of failure, the task remains in the database and thus can be re-attempted manually
+    (via `grouprisectl repeat_tasks`).
+
+    The task (function) to be executed depends on the model of the given object.
+    The mapping from object/model to function is configured in GROUPRISE_REPEATABLE_TASKS.
+    """
+    # store the task temporarily - it will be removed below in case of success
     task = RepeatableTask.objects.create(
-            content_type=model.content_type, object_id=model.id)
+        content_type=obj.content_type, object_id=obj.id
+    )
 
-    model_class_label = model._meta.label
-    task_method_name = settings.GROUPRISE_REPEATABLE_TASKS[model_class_label]
-    task_method = import_string(task_method_name)
-    task_method(model)
-
+    # retrieve the function configured for this object type
+    obj_class_label = obj._meta.label
+    func_name = settings.GROUPRISE_REPEATABLE_TASKS[obj_class_label]
+    func = import_string(func_name)
+    # The execution of the taskmay fail.  Problems are handled by our caller.
+    func(obj)
+    # The task finished successfully, thus we can remove it from the database.
     task.delete()
