9c69b102564226fae5210da5ff346e9640389ae1
Lars Kruse <devel@sumpfralle.de>
2021-02-01 22:41:19 +0100

    Feed import: migrate cron job to periodic huey task
    See #657

--- debian/grouprise.cron.d
+++ debian/grouprise.cron.d
@@ -1,5 +1,2 @@
-# import content from external websites
-46  * * * * _grouprise        chronic grouprisectl import_feeds
-
 # update search index
 */5 * * * * _grouprise        chronic grouprisectl update_index --remove
---
+++ grouprise/features/imports/tasks.py
@@ -0,0 +1,15 @@
+import logging
+
+from huey import crontab
+from huey.contrib.djhuey import db_periodic_task
+
+from grouprise.features.imports.feeds import run_feed_import_for_groups
+
+
+logger = logging.getLogger(__name__)
+
+
+@db_periodic_task(crontab(minute="46"))
+def import_feed_for_groups():
+    logger.info("Update website feeds for groups")
+    run_feed_import_for_groups()
