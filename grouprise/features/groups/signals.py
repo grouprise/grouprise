76813562cc0dcb570df8656f948264e2b52d6b88
Robert <rw@roko.li>
2021-04-20 10:19:32 +0200

    fix: Black format file

--- grouprise/features/groups/signals.py
+++ grouprise/features/groups/signals.py
@@ -3,15 +3,14 @@ import logging
 import subprocess
 
 from django.conf import settings
-from django.core.serializers import serialize
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
-import grouprise.core
 from grouprise.core.utils import slugify
 from grouprise.features.gestalten import models as gestalten
 from grouprise.features.stadt.forms import ENTITY_SLUG_BLACKLIST
 from .models import Group
+from ...core.models import get_unique_slug
 
 logger = logging.getLogger(__name__)
 
@@ -19,13 +18,13 @@ logger = logging.getLogger(__name__)
 def call_hook_script(event_type: str, group: Group):
     try:
         hook_script_name = settings.GROUPRISE.get("HOOK_SCRIPT_PATH", "true")
-        hook_event_info_json = json.dumps({
-            'eventType': event_type,
-            'objectType': 'Group',
-            'objectData': {
-                'id': group.id,
-                'slug': group.slug
-            }})
+        hook_event_info_json = json.dumps(
+            {
+                "eventType": event_type,
+                "objectType": "Group",
+                "objectData": {"id": group.id, "slug": group.slug},
+            }
+        )
         subprocess.run([hook_script_name, hook_event_info_json])
     except FileNotFoundError:
         logger.error("Could not call hook script.")
@@ -39,11 +38,13 @@ def post_group_save(sender, instance, created, **kwargs):
         call_hook_script("changed", instance)
 
     if created:
-        instance.slug = grouprise.core.models.get_unique_slug(
-            Group, {'slug': slugify(instance.name)},
+        instance.slug = get_unique_slug(
+            Group,
+            {"slug": slugify(instance.name)},
             reserved_slugs=ENTITY_SLUG_BLACKLIST,
             reserved_slug_qs=gestalten.Gestalt.objects,
-            reserved_slug_qs_field='user__username')
+            reserved_slug_qs_field="user__username",
+        )
         instance.save()
 
 
