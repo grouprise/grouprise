db0dc90cd43732e388ecd055209415d6e4c1ee69
Lars Kruse <devel@sumpfralle.de>
2021-06-20 21:35:58 +0200

    build: use absolute filename for configuration
    A few actions (e.g. `python3 manage.py compilemesages`) are executed
    in a different directory (due to their limited configurability).
    This causes problems with the discovery of grouprise's configuration
    file (e.g. `grouprise.yaml` is not deterministic anymore).
    
    In order to user the same configuration file for all actions,
    we need to configure the configuration filename via the environment
    variable `GROUPRISE_CONFIG`.

--- make.d/app.mk
+++ make.d/app.mk
@@ -1,4 +1,4 @@
-CONFIG_APP_SETUP = grouprise.yaml
+CONFIG_APP_SETUP ?= grouprise.yaml
 CONFIG_APP_SETUP_TEMPLATE = grouprise.yaml.development
 
 
@@ -7,29 +7,29 @@ $(CONFIG_APP_SETUP): $(GROUPRISE_MAKEFILES)
 
 .PHONY: app_migrate
 app_migrate: virtualenv-check
-	( . "$(ACTIVATE_VIRTUALENV)" && "$(PYTHON_BIN)" manage.py migrate )
+	( . "$(ACTIVATE_VIRTUALENV)" && GROUPRISE_CONFIG=$(abspath $(CONFIG_APP_SETUP)) "$(PYTHON_BIN)" manage.py migrate )
 
 # makefilet runs "manage.py check" during "virtualenv-check" - thus we need the settings beforehand
 virtualenv-check: app_local_settings
 
 .PHONY: app_run
 app_run: app_migrate app_collect_static app_compile_translations
-	( . "$(ACTIVATE_VIRTUALENV)" && "$(PYTHON_BIN)" manage.py runserver )
+	( . "$(ACTIVATE_VIRTUALENV)" && GROUPRISE_CONFIG=$(abspath $(CONFIG_APP_SETUP)) "$(PYTHON_BIN)" manage.py runserver )
 
 .PHONY: app_collect_static
 app_collect_static: virtualenv-check assets
-	( . "$(ACTIVATE_VIRTUALENV)" && "$(PYTHON_BIN)" manage.py collectstatic --no-input )
+	( . "$(ACTIVATE_VIRTUALENV)" && GROUPRISE_CONFIG=$(abspath $(CONFIG_APP_SETUP)) "$(PYTHON_BIN)" manage.py collectstatic --no-input )
 
 .PHONY: app_local_settings
 app_local_settings: $(CONFIG_APP_SETUP)
 
 .PHONY: app_collect_translation_strings
 app_collect_translation_strings:
-	( . "$(ACTIVATE_VIRTUALENV)" && cd grouprise && "$(PYTHON_BIN)" ../manage.py makemessages --no-location )
+	( . "$(ACTIVATE_VIRTUALENV)" && cd grouprise && GROUPRISE_CONFIG=$(abspath $(CONFIG_APP_SETUP)) "$(PYTHON_BIN)" ../manage.py makemessages --no-location )
 
 .PHONY: app_compile_translations
 app_compile_translations:
-	( . "$(ACTIVATE_VIRTUALENV)" && cd grouprise && "$(PYTHON_BIN)" ../manage.py compilemessages )
+	( . "$(ACTIVATE_VIRTUALENV)" && cd grouprise && GROUPRISE_CONFIG=$(abspath $(CONFIG_APP_SETUP)) "$(PYTHON_BIN)" ../manage.py compilemessages )
 
 .PHONY: app_translate
 app_translate:
