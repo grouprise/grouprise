20d578c8ee0771ca2bc2d8ec79241b5683605240
Lars Kruse <devel@sumpfralle.de>
2021-06-30 03:56:09 +0200

    feat(settings): add temporary support for legacy configuration format
    The existence of the previous configuration file
    (/etc/grouprise/settings.py) is tested before loading settings.
    In case this file exists, it will be loaded as a module.
    A warning message is emitted in order to encourage migration to the new
    format.
    
    This should ensure that upgrades work without manual adjustments.

--- grouprise/settings.py
+++ grouprise/settings.py
@@ -1,5 +1,23 @@
-from grouprise.common_settings import *  # noqa: F401 F403
-from grouprise.settings_loader import import_settings_from_yaml
+import os
+import sys
 
+LEGACY_CONFIG_FILENAME = "/etc/grouprise/settings.py"
 
-import_settings_from_yaml(locals())
+if os.path.exists(LEGACY_CONFIG_FILENAME):
+    # temporary support for the old configuration mechanism
+    print(
+        f"WARNING: the deprecated grouprise configuration file {LEGACY_CONFIG_FILENAME} exists. "
+        "It will be used for now, but you should migrate to the new configuration format soon. "
+        "See https://docs.grouprise.org/releases/4.0.html for details.",
+        file=sys.stderr,
+    )
+    sys.path.insert(0, os.path.dirname(LEGACY_CONFIG_FILENAME))
+    from settings import *  # noqa: F401 F403
+
+    sys.path.pop(0)
+else:
+    # use the new configuration format
+    from grouprise.common_settings import *  # noqa: F401 F403
+    from grouprise.settings_loader import import_settings_from_yaml
+
+    import_settings_from_yaml(locals())
