92e40409f16fa37e749ffda1b49056e1177d70c0
Lars Kruse <devel@sumpfralle.de>
2021-07-04 22:57:11 +0200

    feat(matrix_chat): hide the public room from non-members if it is empty
    We do want visitors to walk into an embarrassingly deserted room.

--- grouprise/features/matrix_chat/rules.py
+++ grouprise/features/matrix_chat/rules.py
@@ -8,7 +8,13 @@ def is_matrix_chat_enabled():
     return MATRIX_SETTINGS.ENABLED
 
 
+@rules.predicate
+def matrix_chat_room_has_members(room):
+    return room and (room.members_count > 0)
+
+
 # Evaluating this rule returns False if the "matrix_chat" application is not installed
 # (see INSTALLED_APPS).  It returns True, if the application is installed and the corresponding
 # setting is True.
 rules.add_rule("is_matrix_chat_enabled", is_matrix_chat_enabled)
+rules.add_rule("matrix_chat.room_is_populated", matrix_chat_room_has_members)
