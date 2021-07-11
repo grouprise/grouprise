93db5a79426db1475ac7f459e94c8d308265fa6c
Lars Kruse <devel@sumpfralle.de>
2021-07-11 15:59:34 +0200

    docs: add missing file types
    The generated CSS and PNG files were missing from doc build.
    One visible effect was the missing graphviz graph in the deployment
    documentation page.

--- make.d/doc.mk
+++ make.d/doc.mk
@@ -1,4 +1,5 @@
 DIR_BUILD_DOC = $(abspath $(DIR_BUILD)/doc)
+SPHINX_WANTED_FILE_EXTENSIONS = html js css png
 
 
 .PHONY: doc
@@ -12,7 +13,7 @@ build: doc
 .PHONY: install-doc
 install-doc: doc
 	cd "$(DIR_BUILD_DOC)/html" \
-		&& { find -type f -name "*.html" -or -name "*.js"; find _static -type f; } \
+		&& { find -type f -false $(patsubst %,-or -name "*.%",$(SPHINX_WANTED_FILE_EXTENSIONS)); find _static -type f; } \
 			| sort | uniq | while read -r fname; do \
 				install -D --target-directory "$(abspath $(DESTDIR))/usr/share/doc/grouprise/html/$$(dirname "$$fname")" "$$fname"; done
 
