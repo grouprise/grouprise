0d5bb2b6a36994f55a31fbf412d7043371299324
Lars Kruse <devel@sumpfralle.de>
2021-10-31 02:13:31 +0100

    build: update makefilet to v0.9.1
    This fixes at least a race condition for "make release-*".

--- make.d/makefilet-download-ondemand.mk
+++ make.d/makefilet-download-ondemand.mk
@@ -3,26 +3,28 @@
 #   * project-wide inclusion as a git-submodule (or code copy)
 #   * on-demand download (temporarily stored in the local 'build/' directory)
 
-MAKEFILET_DOWNLOAD_VERSION = 0.7.2
-MAKEFILET_DOWNLOAD_URL ?= https://notabug.org/sumpfralle/makefilet/archive/v$(MAKEFILET_DOWNLOAD_VERSION).tar.gz
+# alternative version: "master" (the tip of the current development branch)
+MAKEFILET_DOWNLOAD_VERSION ?= v0.9.1
+MAKEFILET_DOWNLOAD_URL_TEMPLATE ?= https://notabug.org/sumpfralle/makefilet/archive/__VERSION__.tar.gz
+MAKEFILET_DOWNLOAD_URL ?= $(subst __VERSION__,$(MAKEFILET_DOWNLOAD_VERSION),$(MAKEFILET_DOWNLOAD_URL_TEMPLATE))
 
 # first attempt: system-wide installation (e.g. deb package) or submodule of this project?
 -include makefilet/main.mk
-ifeq ($(DIR_MAKEFILET), )
+ifndef DIR_MAKEFILET
 # we failed - it is not available globally or as a submodule
 # second attempt: include a downloaded makefilet archive
 DIR_BUILD ?= build
 DIR_MAKEFILET_DOWNLOAD = $(DIR_BUILD)/makefilet
 -include $(DIR_MAKEFILET_DOWNLOAD)/main.mk
-ifeq ($(DIR_MAKEFILET), )
+ifndef DIR_MAKEFILET
 # third attempt: download and extract a known release
 $(info Downloading 'makefilet' ...)
 $(shell mkdir -p "$(DIR_MAKEFILET_DOWNLOAD)" \
-	&& wget -q -O - "$(MAKEFILET_DOWNLOAD_URL)" \
+	&& wget --no-verbose --output-document - "$(MAKEFILET_DOWNLOAD_URL)" \
 	| tar -xz -C "$(DIR_MAKEFILET_DOWNLOAD)" --strip-components=1 -f -)
 # last include attempt
 -include $(DIR_MAKEFILET_DOWNLOAD)/main.mk
-ifeq ($(DIR_MAKEFILET), )
+ifndef DIR_MAKEFILET
 $(error Failed to initialize 'makefilet'. It seems like it is neither installed system-wide, as a submodule or available via download.)
 endif
 endif
