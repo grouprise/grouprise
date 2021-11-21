8db3b7b370080b3f018597f9103a55593dc4a41f
Lars Kruse <devel@sumpfralle.de>
2021-11-21 22:57:56 +0100

    feat(matrix_commander): add interactive console
    Run  in order to
    interact with the MatrixCommander bot on a console instead of using a chat room.

--- grouprise/features/matrix_commander/management/commands/matrix_commander.py
+++ grouprise/features/matrix_commander/management/commands/matrix_commander.py
@@ -1,8 +1,13 @@
 import asyncio
 import logging
 
+import kien.command.help
+import kien.command.quit
+from kien.runner import ConsoleRunner
+
 from django.core.management.base import BaseCommand
 
+from grouprise.features.matrix_commander.commands import commander
 from grouprise.features.matrix_commander.matrix_bot import CommanderBot
 
 
@@ -23,8 +28,32 @@ class Command(BaseCommand):
             choices=set(self.LOG_LEVEL_MAP),
             help="Specify a log level",
         )
+        parser.add_argument(
+            "--console",
+            action="store_true",
+            help="Run an interactive session on the console (useful for debugging).",
+        )
 
     def handle(self, *args, log_level=None, **options):
         logging.basicConfig(level=self.LOG_LEVEL_MAP[log_level])
-        bot = CommanderBot()
-        asyncio.run(bot.serve_forever())
+        if options["console"]:
+            self.serve_console()
+        else:
+            bot = CommanderBot()
+            asyncio.run(bot.serve_forever())
+
+    def serve_console(self):
+        console = CommanderConsole()
+        console.commander = commander
+        console.commander.compose(
+            kien.command.help.command,
+            kien.command.quit.command,
+        )
+        console.run()
+
+
+class CommanderConsole(ConsoleRunner):
+    def parse_args(self, args=None):
+        if args is None:
+            args = []
+        return self.get_arg_parser().parse_args(args)
