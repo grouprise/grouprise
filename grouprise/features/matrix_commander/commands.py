d6622d8dc7fa0268daf1c7ecfd35a9220516f102
Lars Kruse <devel@sumpfralle.de>
2021-11-22 05:17:47 +0100

    refactor(matrix_commander): use decorators for input conversion
    This simplifies the code for functions dealing with users, groups and
    content.

--- grouprise/features/matrix_commander/commands.py
+++ grouprise/features/matrix_commander/commands.py
@@ -1,4 +1,5 @@
 import difflib
+import functools
 import os
 
 from grouprise.features.associations.models import Association
@@ -20,18 +21,89 @@ class MultilineResult(CommandResult):
         super().__init__(os.linesep.join(response_lines), *args, **kwargs)
 
 
+class ResolveException(Exception):
+    def __init__(self, message_lines, *args, **kwargs):
+        if isinstance(message_lines, str):
+            message_lines = [message_lines]
+        self._msg_lines = tuple(message_lines)
+        super().__init__(self, os.linesep.join(self._msg_lines))
+
+    def get_command_results(self):
+        for line in self._msg_lines:
+            yield CommandResult(line, success=False)
+
+
+def _get_invalid_key_response(invalid_key, label, alternatives, max_alternative_count):
+    yield f"Unknown {label}: {invalid_key}"
+    closest_matches = difflib.get_close_matches(
+        invalid_key, alternatives, max_alternative_count
+    )
+    if closest_matches:
+        yield "Maybe you meant one of the following?"
+        for item in closest_matches:
+            yield "  " + item
+
+
 def get_unknown_username_response(username, max_alternative_count=10):
     all_usernames = (
         item.user.username for item in Gestalt.objects.only("user__username")
     )
-    yield CommandResult(f"Unknown username: {username}", success=False)
-    closest_matches = difflib.get_close_matches(
-        username, all_usernames, max_alternative_count
+    yield from _get_invalid_key_response(
+        username, "username", all_usernames, max_alternative_count
     )
-    if closest_matches:
-        yield CommandResult("Maybe you meant one of the following?", success=False)
-        for item in closest_matches:
-            yield CommandResult("  " + item, success=False)
+
+
+def get_unknown_groupname_response(groupname, max_alternative_count=10):
+    all_groupnames = (item.slug for item in Group.objects.only("slug"))
+    yield from _get_invalid_key_response(
+        groupname, "groupname", all_groupnames, max_alternative_count
+    )
+
+
+def inject_resolved(source, target, resolvers):
+    def outer(func):
+        @functools.wraps(func)
+        def inner(*args, **kwargs):
+            collected_errors = []
+            original_value = kwargs.pop(source)
+            for resolver in resolvers:
+                try:
+                    new_value = resolver(original_value)
+                except ResolveException as exc:
+                    collected_errors.append(exc.get_command_results())
+                else:
+                    # the conversion is finished successfully
+                    kwargs[target] = new_value
+                    break
+            else:
+                # none of the resolvers managed to deliver a result - bail out with the first error
+                yield from collected_errors[0]
+                return
+            # one conversion was successfuly
+            yield from func(*args, **kwargs)
+
+        return inner
+
+    return outer
+
+
+def get_gestalt(name: str) -> Gestalt:
+    try:
+        return Gestalt.objects.get(user__username=name)
+    except Gestalt.DoesNotExist:
+        raise ResolveException(get_unknown_username_response(name))
+
+
+def get_group(slug_or_url: str) -> Group:
+    if "/" in slug_or_url:
+        # the token in the URL is the group slug
+        group_name = slug_or_url.strip("/").split("/")[-1]
+    else:
+        group_name = slug_or_url
+    try:
+        return Group.objects.get(slug=group_name)
+    except Group.DoesNotExist:
+        raise ResolveException(get_unknown_groupname_response(group_name))
 
 
 @commander("user", is_abstract=True)
@@ -57,30 +129,22 @@ def user_list_unused(limit: int = 5):
 
 
 @commander(user, "show", var("username"))
-def show_user(username):
-    try:
-        gestalt = Gestalt.objects.get(user__username=username)
-    except Gestalt.DoesNotExist:
-        yield from get_unknown_username_response(username)
-    else:
-        yield CommandResult(f"Username: {gestalt.user.username}")
-        yield CommandResult(f"Email: {gestalt.user.email}")
-        yield CommandResult(f"Contributions: {gestalt.contributions.count()}")
-        yield CommandResult(f"Group memberships: {gestalt.memberships.count()}")
-        latest_contribution = gestalt.contributions.order_by("time_created").last()
-        timestamp = latest_contribution.time_created if latest_contribution else None
-        yield CommandResult(f"Latest contribution: {timestamp}")
+@inject_resolved(source="username", target="gestalt", resolvers=[get_gestalt])
+def show_user(gestalt: Gestalt):
+    yield CommandResult(f"Username: {gestalt.user.username}")
+    yield CommandResult(f"Email: {gestalt.user.email}")
+    yield CommandResult(f"Contributions: {gestalt.contributions.count()}")
+    yield CommandResult(f"Group memberships: {gestalt.memberships.count()}")
+    latest_contribution = gestalt.contributions.order_by("time_created").last()
+    timestamp = latest_contribution.time_created if latest_contribution else None
+    yield CommandResult(f"Latest contribution: {timestamp}")
 
 
 @commander(user, "remove", var("username"))
-def remove_user(username):
-    try:
-        gestalt = Gestalt.objects.get(user__username=username)
-    except Gestalt.DoesNotExist:
-        yield from get_unknown_username_response(username)
-    else:
-        gestalt.delete()
-        yield CommandResult(f"Removed user '{gestalt}'")
+@inject_resolved(source="username", target="gestalt", resolvers=[get_gestalt])
+def remove_user(gestalt):
+    gestalt.delete()
+    yield CommandResult(f"Removed user '{gestalt}'")
 
 
 @commander("content", is_abstract=True)
@@ -89,6 +153,9 @@ def content():
 
 
 def get_association_by_url(url):
+    not_found_exception = ResolveException(
+        f"Failed to find content based on URL ('{url}'). Maybe try the permanent link?"
+    )
     base_url = get_grouprise_baseurl().rstrip("/")
     url = url.rstrip("/")
     if url.startswith(base_url):
@@ -99,7 +166,7 @@ def get_association_by_url(url):
         try:
             association_pk = int(tokens[2])
         except ValueError:
-            return None
+            raise not_found_exception
         else:
             return Association.objects.get(pk=association_pk)
     elif len(tokens) == 2:
@@ -112,18 +179,19 @@ def get_association_by_url(url):
             try:
                 entity = Gestalt.objects.get(slug=entity_slug)
             except Gestalt.DoesNotExist:
-                return None
+                raise not_found_exception
         for association in Association.objects.filter(slug=association_slug):
             if association.entity == entity:
                 return association
         else:
-            return None
+            raise not_found_exception
     else:
-        return None
+        raise not_found_exception
 
 
 @commander(content, "visibility", var("url"), var("state"))
-def change_content_visibility(url, state):
+@inject_resolved(source="url", target="association", resolvers=[get_association_by_url])
+def change_content_visibility(association: Association, state: str):
     if state.lower() == "public":
         should_go_public = True
     elif state.lower() == "private":
@@ -133,20 +201,14 @@ def change_content_visibility(url, state):
             f"Invalid target state: {state} (should be 'public' or 'private')"
         )
         return
-    association = get_association_by_url(url)
-    if association is None:
-        yield CommandResult(
-            f"Failed to find content based on URL ('{url}'). Maybe try the permanent link?"
-        )
+    if should_go_public:
+        association.public = True
     else:
-        if should_go_public:
-            association.public = True
-        else:
-            association.public = False
-        association.save()
-        yield CommandResult(
-            f"Changed visibility to {'public' if should_go_public else 'private'}"
-        )
+        association.public = False
+    association.save()
+    yield CommandResult(
+        f"Changed visibility to {'public' if should_go_public else 'private'}"
+    )
 
 
 class MatrixCommander:
