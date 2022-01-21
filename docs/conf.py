9157232c3a42055e53751066e688fa4942db792d
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2022-01-21 14:30:29 +0100

    Bump version: 4.7.0 -> 4.7.1

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = 'grouprise contributors'
 author = 'grouprise contributors'
 
 # The short X.Y version
-version = '4.7.0'
+version = '4.7.1'
 # The full version, including alpha/beta/rc tags
-release = '4.7.0'
+release = '4.7.1'
 
 
 # -- General configuration ---------------------------------------------------
