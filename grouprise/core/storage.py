aabc7c59c82345e8b4303a02b801ea92db2aba55
Lars Kruse <devel@sumpfralle.de>
2022-03-22 01:07:09 +0100

    refactor(storage): clarify set of allowed characters
    In addition we use `random.choices` instead of `secrets.choice`, since
    the former can generate the full string (instead of single characters)
    and we do not need the predictability properties offered by `secrets`.

--- grouprise/core/storage.py
+++ grouprise/core/storage.py
@@ -1,5 +1,5 @@
 import os.path
-import secrets
+import random
 import string
 from pathlib import Path
 
@@ -8,6 +8,7 @@ from django.core.files.storage import FileSystemStorage
 
 class Storage(FileSystemStorage):
     FILE_ROOT_LENGTH = 16
+    VALID_CHARACTERS = string.ascii_lowercase + string.digits
 
     def get_available_name(self, name, max_length=None):
         dir_name, file_name = os.path.split(name)
@@ -15,9 +16,8 @@ class Storage(FileSystemStorage):
         file_ext = os.path.splitext(file_name)[1].lower()
         result = None
         while result is None:
-            alphabet = string.ascii_lowercase + string.digits
             file_root = "".join(
-                secrets.choice(alphabet) for _ in range(self.FILE_ROOT_LENGTH)
+                random.choices(self.VALID_CHARACTERS, k=self.FILE_ROOT_LENGTH)
             )
             result = Path(dir_name) / f"{file_root}{file_ext}"
             if self.exists(result):
