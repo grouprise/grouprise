10efe34567bfcd8d0f2d96989d95e145f846a182
Robert <rw@roko.li>
2022-06-01 10:20:50 +0200

    refactor: add enum for audience keys

--- grouprise/features/notifications/signals.py
+++ grouprise/features/notifications/signals.py
@@ -1,4 +1,5 @@
-from typing import Union
+from enum import Enum, auto
+from typing import Union, Iterable, Iterator, Optional, Mapping
 
 from django.dispatch import receiver
 from huey.contrib.djhuey import db_task
@@ -20,7 +21,19 @@ from grouprise.features.matrix_chat.signals import send_matrix_notifications
 
 
 class AffectedGestalten:
-    def __init__(self, instance: Union[Content, Contribution]):
+    class Audience(Enum):
+        # all gestalten affected by changes to the given instance
+        ALL = auto()
+        # the gestalt to whom this content is associated, if any
+        ASSOCIATED_GESTALT = auto()
+        # the author of the initial contribution, if any
+        INITIAL_CONTRIBUTOR = auto()
+        # all gestalten who subscribed to the associated group, if any
+        GROUP_SUBSCRIBERS = auto()
+        # all members of the associated group who have a subscription
+        SUBSCRIBED_GROUP_MEMBERS = auto()
+
+    def __init__(self, instance: Union[Content, Contribution]) -> None:
         self.instance = instance
         self.container: Union[Content, Conversation] = (
             instance if isinstance(instance, Content) else instance.container
@@ -29,42 +42,46 @@ class AffectedGestalten:
         self.is_group_context = isinstance(self.association.entity, Group)
         self.gestalten = self._get_affected_gestalten()
 
-    def __contains__(self, item):
+    def __contains__(self, item: Audience) -> bool:
         return self.gestalten.__contains__(item)
 
-    def __getitem__(self, item):
+    def __getitem__(self, item: Audience) -> Iterable[Gestalt]:
         return self.gestalten[item]
 
-    def __iter__(self):
-        return self.gestalten["all"].__iter__()
+    def __iter__(self) -> Iterator[Gestalt]:
+        return self.gestalten[self.Audience.ALL].__iter__()
 
-    def get_initial_contributor(self):
+    def get_initial_contributor(self) -> Optional[Gestalt]:
         if not self.container.contributions.exists():
             return None
         return self.container.contributions.first().author
 
-    def _get_affected_gestalten(self):
+    def _get_affected_gestalten(self) -> Mapping[Audience, Iterable[Gestalt]]:
         gestalten = {}
         initial_contributor = self.get_initial_contributor()
         if initial_contributor:
-            gestalten["initial_contributor"] = {initial_contributor}
+            gestalten[self.Audience.INITIAL_CONTRIBUTOR] = {initial_contributor}
         if self.is_group_context:
             gestalten.update(
                 {
-                    "group_subscribers": self.association.entity.subscribers.all(),
-                    "subscribed_group_members": self.association.entity.members.filter(
-                        subscriptions__group=self.association.entity
+                    self.Audience.GROUP_SUBSCRIBERS: (
+                        self.association.entity.subscribers.all()
+                    ),
+                    self.Audience.SUBSCRIBED_GROUP_MEMBERS: (
+                        self.association.entity.members.filter(
+                            subscriptions__group=self.association.entity
+                        )
                     ),
                 }
             )
         else:
             gestalten.update(
                 {
-                    "associated_gestalt": {self.association.entity},
+                    self.Audience.ASSOCIATED_GESTALT: {self.association.entity},
                 }
             )
         return {
-            "all": {g for subset in gestalten.values() for g in subset},
+            self.Audience.ALL: {g for subset in gestalten.values() for g in subset},
             **gestalten,
         }
 
@@ -90,24 +107,31 @@ class EmailNotifications:
     def send(self):
         kwargs = {"association": self.affected_gestalten.association}
         if self.affected_gestalten.association.public:
-            self.send_to("group_subscribers", is_subscriber=True, **kwargs)
+            self.send_to(
+                AffectedGestalten.Audience.GROUP_SUBSCRIBERS,
+                is_subscriber=True,
+                **kwargs,
+            )
         else:
             if self.affected_gestalten.is_group_context:
-                self.send_to("subscribed_group_members", is_subscriber=True, **kwargs)
+                self.send_to(
+                    AffectedGestalten.Audience.SUBSCRIBED_GROUP_MEMBERS,
+                    is_subscriber=True,
+                    **kwargs,
+                )
             else:
-                self.send_to("associated_gestalt", **kwargs)
+                self.send_to(AffectedGestalten.Audience.ASSOCIATED_GESTALT, **kwargs)
             if self._is_initial_contributor_external():
-                self.send_to("initial_contributor", **kwargs)
+                self.send_to(AffectedGestalten.Audience.INITIAL_CONTRIBUTOR, **kwargs)
 
-    def send_to(self, audience_key: str, **kwargs):
-        if audience_key in self.affected_gestalten:
-            for recipient in self.affected_gestalten[audience_key]:
-                if (
-                    recipient not in self.recipients_to_ignore
-                    and self.does_recipient_want_notifications(recipient)
-                ):
-                    self.send_notification(recipient, **kwargs)
-                    self.recipients_to_ignore.add(recipient)
+    def send_to(self, audience: AffectedGestalten.Audience, **kwargs):
+        for recipient in self.affected_gestalten[audience]:
+            if (
+                recipient not in self.recipients_to_ignore
+                and self.does_recipient_want_notifications(recipient)
+            ):
+                self.send_notification(recipient, **kwargs)
+                self.recipients_to_ignore.add(recipient)
 
     def send_notification(self, recipient, **kwargs):
         self.notification_class(self.affected_gestalten.instance).send(
