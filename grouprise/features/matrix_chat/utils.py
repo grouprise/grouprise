grouprise/grouprise

commit 7645a6ef87ee196bdc549cc112038487af7183fb

refactor(matrix_chat): allow room creation in sync context
The function is only used by sync callers, thus we better reverse the
sync/async context switches in that function.

grouprise/features/matrix_chat/utils.py
@@ -40,7 +40,7 @@ def get_gestalt_matrix_notification_room(
         ).value
     except ObjectDoesNotExist:
         if create_if_missing:
-            return async_to_sync(create_gestalt_matrix_notification_room)(gestalt)
+            return create_gestalt_matrix_notification_room(gestalt)
         else:
             return None
 
@@ -69,32 +69,37 @@ def delete_gestalt_matrix_notification_room(gestalt):
     ).delete()
 
 
-async def create_gestalt_matrix_notification_room(bot, gestalt) -> str:
+def create_gestalt_matrix_notification_room(gestalt: Gestalt) -> str:
     """invite the user into a new room and store the room ID
 
     Raises MatrixError in case of problems.
     """
+
+    async def _create_private_room():
+        async with ChatBot() as bot:
+            room_id = await bot.create_private_room(room_title, room_description)
+            # raise the default power level for new members to "moderator"
+            await bot._change_room_state(
+                room_id,
+                {"users_default": 50},
+                "m.room.power_levels",
+                room_label=room_label,
+            )
+            # invite the target user
+            await bot.invite_into_room(room_id, gestalt, room_label)
+            return room_id
+
     room_title = _("{site_name} - notifications").format(
-        site_name=(await sync_to_async(get_grouprise_site)()).name
+        site_name=get_grouprise_site().name
     )
     room_description = _("Notifications for private messages from {site_url}").format(
-        site_url=await sync_to_async(get_grouprise_baseurl)()
+        site_url=get_grouprise_baseurl()
     )
     # the label is used for log messages only
-    gestalt_label = await sync_to_async(str)(gestalt)
+    gestalt_label = str(gestalt)
     room_label = f"notifications for {gestalt_label}"
-    # create the room
-    room_id = await bot.create_private_room(room_title, room_description)
-    # raise the default power level for new members to "moderator"
-    await bot._change_room_state(
-        room_id,
-        {"users_default": 50},
-        "m.room.power_levels",
-        room_label=room_label,
-    )
-    # invite the target user
-    await bot.invite_into_room(room_id, gestalt, room_label)
-    await sync_to_async(set_gestalt_matrix_notification_room)(gestalt, room_id)
+    room_id = async_to_sync(_create_private_room())()
+    set_gestalt_matrix_notification_room(gestalt, room_id)
     return room_id
 
 

commit 3fac68bfaa5b21c398880697d6873fe245f809df

fix: create private matrix notification room for users
The room creation was broken in commit 7645a6ef87ee1.

grouprise/features/matrix_chat/utils.py
@@ -96,7 +96,7 @@ def create_gestalt_matrix_notification_room(gestalt: Gestalt) -> str:
     # the label is used for log messages only
     gestalt_label = str(gestalt)
     room_label = f"notifications for {gestalt_label}"
-    room_id = async_to_sync(_create_private_room())()
+    room_id = async_to_sync(_create_private_room)()
     set_gestalt_matrix_notification_room(gestalt, room_id)
     return room_id
 