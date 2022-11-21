e490c70057942ad7e084237cec445b55c52e7440
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2022-11-21 13:51:39 +0100

    chore: ease learning curve for frontend-devs
    It’s nice to keep existing tooling workflows straight and simple, so
    `npm run build` and `npm run dev` should just work without someone
    having to learn about make and how we dump google fonts with it.

--- make.d/assets.mk
+++ make.d/assets.mk
@@ -1,20 +1,11 @@
 DIR_STATIC = $(DIR_BUILD)/static
 DIR_RES = res
-DIR_FONTS = $(DIR_RES)/fonts
 DIR_NODE = node_modules
-DIR_NODE_BIN ?= $(DIR_NODE)/.bin
 STAMP_NODE_MODULES = $(DIR_NODE)/.stamp-install
 
 BIN_NPM ?= npm
-BIN_WEBPACK = $(DIR_NODE_BIN)/webpack
-BIN_FONTDUMP = $(DIR_NODE_BIN)/fontdump
 
 DEPS_ASSETS = $(shell find "$(DIR_RES)" -type f)
-DEPS_FONTS = $(shell find "$(DIR_FONTS)" -type f)
-
-# google fonts
-GOOGLE_FONTS_CSS = $(DIR_BUILD)/fonts/google/fonts.css
-URL_FONT_GOOGLE = https://fonts.googleapis.com/css?family=Roboto+Slab:300,400,700|Roboto:300,400,400i,500,700
 
 # static output files
 STAMP_STATIC_WEBPACK = $(DIR_BUILD)/.static_webpack
@@ -23,22 +14,15 @@ $(STAMP_NODE_MODULES): package.json
 	"$(BIN_NPM)" ci --no-progress
 	touch "$(STAMP_NODE_MODULES)"
 
-$(GOOGLE_FONTS_CSS): $(STAMP_NODE_MODULES)
-	"$(BIN_FONTDUMP)" \
-		--target-directory "$(dir $(GOOGLE_FONTS_CSS))" \
-		--web-directory "." \
-		"$(URL_FONT_GOOGLE)"
 
-$(STAMP_STATIC_WEBPACK): $(GOOGLE_FONTS_CSS) $(DEPS_ASSETS) webpack.config.js
+$(STAMP_STATIC_WEBPACK): $(DEPS_ASSETS) webpack.config.js
 	mkdir -p "$(DIR_STATIC)"
 	"$(BIN_NPM)" run build
 	touch "$(STAMP_STATIC_WEBPACK)"
 
-.PHONY: assets_fonts
-assets_fonts: $(GOOGLE_FONTS_CSS)
 
 .PHONY: assets_webpack
 assets_webpack: $(STAMP_STATIC_WEBPACK)
 
 .PHONY: assets
-assets: assets_fonts assets_webpack
+assets: assets_webpack
