7881037a108d0eb4cb33fb90f12f0812c57fa7e6
Lars Kruse <devel@sumpfralle.de>
2023-09-07 17:29:55 +0200

    fix: re-order app startup initialization
    Previously the import of the `on_startup` decorator from `huey`
    triggered huey's initialization.
    This would conflict with a not-yet-resolved Django setting for huey
    (e.g. the filename of the database).
    Thus, now we try to resolve the configuration before importing the huey
    module.

--- grouprise/core/apps.py
+++ grouprise/core/apps.py
@@ -2,33 +2,72 @@ import sys
 
 from django.apps import AppConfig
 from django.conf import settings
-from huey.contrib.djhuey import on_startup
 from setproctitle import setproctitle
 
 from grouprise import __release__
 
 
-@on_startup()
-def _resolve_lazy_core_settings():
-    """we need to ensure, that all settings are resolved (required for async tasks)
+def _register_startup_hooks():
+    """register the startup hooks after the Django settings were at least partially resolved
 
-    Django (at least up to 4.x) does not allow ORM requests in async contexts.
+    The startup hooks cannot be imported at the top level of this module, since the import of
+    the `huey.contrib.djhuey` module relies on a properly populated settings store.
+
+    Thus, we need to run `_resolve_django_settings` before importing `on_startup`.
     """
+    from huey.contrib.djhuey import on_startup
+
+    @on_startup()
+    def _resolve_lazy_core_settings():
+        """we need to ensure, that all settings are resolved (required for async tasks)
+
+        Django (at least up to 4.x) does not allow ORM requests in async contexts.
+
+        Most settings are probably populated in `_resolve_django_settings`.
+        But some settings may remain unresolved at that (early) moment of app initialization.
+        """
+        from .settings import CORE_SETTINGS
+
+        CORE_SETTINGS.resolve_lazy_settings()
+
+    @on_startup()
+    def _set_process_title_for_tasks():
+        """override the process name for "run_huey"
+
+        This eases process tracking (e.g. via monit) and is simply more beautiful.
+        """
+        # This "on_startup" hook is also called within the context of a regular (uWSGI) process.
+        # Thus, we need to guess, whether we were executed as the task processor.
+        if "run_huey" in sys.argv:
+            setproctitle("grouprise-tasks")
+
+
+def _resolve_django_settings():
     from .settings import CORE_SETTINGS
 
-    CORE_SETTINGS.resolve_lazy_settings()
+    try:
+        CORE_SETTINGS.resolve_lazy_settings()
+    except Exception:
+        # Resolution may fail under certain circumstances, e.g. during the initial run of
+        # "migrate" (before the django site is written to the database).
+        # We just hope, this works *almost* always.
+        pass
 
 
-@on_startup()
-def _set_process_title_for_tasks():
-    """override the process name for "run_huey"
+def _configure_sentry():
+    # configure sentry if DSN has been defined
+    sentry_dsn = getattr(settings, "SENTRY_DSN", None)
+    if sentry_dsn:
+        import sentry_sdk
+        from sentry_sdk.integrations.django import DjangoIntegration
 
-    This eases process tracking (e.g. via monit) and is simply more beautiful.
-    """
-    # This "on_startup" hook is also called within the context of a regular (uWSGI) process.
-    # Thus, we need to guess, whether we were executed as the task processor.
-    if "run_huey" in sys.argv:
-        setproctitle("grouprise-tasks")
+        init_options = {
+            "integrations": [DjangoIntegration()],
+            "send_default_pii": True,
+            "release": f"grouprise@{__release__}",
+        }
+        init_options.update(getattr(settings, "SENTRY_INIT_OPTIONS", {}))
+        sentry_sdk.init(dsn=sentry_dsn, **init_options)
 
 
 class CoreConfig(AppConfig):
@@ -36,27 +75,6 @@ class CoreConfig(AppConfig):
 
     def ready(self):
         self.module.autodiscover()
-
-        from .settings import CORE_SETTINGS
-
-        try:
-            CORE_SETTINGS.resolve_lazy_settings()
-        except Exception:
-            # Resolution may fail under certain circumstances, e.g. during the initial run of
-            # "migrate" (before the django site is written to the database).
-            # We just hope, this works *almost* always.
-            pass
-
-        # configure sentry if DSN has been defined
-        sentry_dsn = getattr(settings, "SENTRY_DSN", None)
-        if sentry_dsn:
-            import sentry_sdk
-            from sentry_sdk.integrations.django import DjangoIntegration
-
-            init_options = {
-                "integrations": [DjangoIntegration()],
-                "send_default_pii": True,
-                "release": f"grouprise@{__release__}",
-            }
-            init_options.update(getattr(settings, "SENTRY_INIT_OPTIONS", {}))
-            sentry_sdk.init(dsn=sentry_dsn, **init_options)
+        _resolve_django_settings()
+        _register_startup_hooks()
+        _configure_sentry()
