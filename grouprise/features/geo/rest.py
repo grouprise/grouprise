857f415bcaae0ff3757bb4acea133ba456a88c3d
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2024-03-20 18:44:47 +0100

    refactor: use default filter method

--- grouprise/features/geo/rest.py
+++ grouprise/features/geo/rest.py
@@ -6,17 +6,20 @@ from rest_framework import serializers
 class HasLocationFilter(django_filters.BooleanFilter):
     def __init__(self, model_cls, *args, **kwargs):
         self.model_cls = model_cls
-        super().__init__(*args, method=self.filter_has_location, **kwargs)
+        super().__init__(*args, **kwargs)
 
-    def filter_has_location(self, queryset, name, value):
+    def filter(self, qs, value):
         from .models import Location
 
-        has_location = models.Exists(
-            Location.objects.for_locatable_type(self.model_cls).filter(
-                locatable_id=models.OuterRef("pk")
-            )
+        if value is None:
+            return qs
+
+        entity_has_location = models.Exists(
+            Location.objects
+            .for_locatable_type(self.model_cls)
+            .filter(locatable_id=models.OuterRef("pk"))
         )
-        return queryset.annotate(has_location=has_location).filter(has_location=value)
+        return qs.annotate(has_location=entity_has_location).filter(has_location=value)
 
 
 class LocationField(serializers.Field):
