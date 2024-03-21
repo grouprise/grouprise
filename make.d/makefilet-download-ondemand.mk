a39579cfa8c0acb96ea29a474e996fdea997e47e
Lars Kruse <devel@sumpfralle.de>
2024-03-21 03:03:13 +0100

    build(makefilet): update makefilet downloader in order to use the pre-configured fallback URLs
    This avoids the hassle of enforcing specific makefilet download sources.

--- make.d/makefilet-download-ondemand.mk
+++ make.d/makefilet-download-ondemand.mk
@@ -3,10 +3,24 @@
 #   * project-wide inclusion as a git-submodule (or code copy)
 #   * on-demand download (temporarily stored in the local 'build/' directory)
 
-# alternative version: "master" (the tip of the current development branch)
+# alternative version: "main" (the tip of the current development branch)
 MAKEFILET_DOWNLOAD_VERSION ?= v0.14.2
-MAKEFILET_DOWNLOAD_URL_TEMPLATE ?= https://notabug.org/sumpfralle/makefilet/archive/__VERSION__.tar.gz
-MAKEFILET_DOWNLOAD_URL ?= $(subst __VERSION__,$(MAKEFILET_DOWNLOAD_VERSION),$(MAKEFILET_DOWNLOAD_URL_TEMPLATE))
+
+ifneq ($(MAKEFILET_DOWNLOAD_URL_TEMPLATE),)
+$(warning 'MAKEFILET_DOWNLOAD_URL_TEMPLATE' is deprecated. Use 'MAKEFILET_DOWNLOAD_URL_TEMPLATES' instead.)
+MAKEFILET_DOWNLOAD_URL_TEMPLATES ?= $(MAKEFILET_DOWNLOAD_URL_TEMPLATE)
+else
+MAKEFILET_DOWNLOAD_URL_TEMPLATES ?= \
+	https://notabug.org/sumpfralle/makefilet/archive/__VERSION__.tar.gz \
+	https://git.hack-hro.de/kmohrf/makefilet/-/archive/__VERSION__/makefilet-__VERSION__.tar.gz
+endif
+
+ifneq ($(MAKEFILET_DOWNLOAD_URL),)
+$(warning 'MAKEFILET_DOWNLOAD_URL' is deprecated. Use 'MAKEFILET_DOWNLOAD_URLS' instead.)
+MAKEFILET_DOWNLOAD_URLS ?= $(MAKEFILET_DOWNLOAD_URL)
+else
+MAKEFILET_DOWNLOAD_URLS ?= $(foreach template,$(MAKEFILET_DOWNLOAD_URL_TEMPLATES),$(subst __VERSION__,$(MAKEFILET_DOWNLOAD_VERSION),$(template)))
+endif
 
 # first attempt: system-wide installation (e.g. deb package) or submodule of this project?
 -include makefilet/main.mk
@@ -20,8 +34,15 @@ ifndef DIR_MAKEFILET
 # third attempt: download and extract a known release
 $(info Downloading 'makefilet' ...)
 $(shell mkdir -p "$(DIR_MAKEFILET_DOWNLOAD)" \
-	&& wget --no-verbose --output-document - "$(MAKEFILET_DOWNLOAD_URL)" \
-	| tar -xz -C "$(DIR_MAKEFILET_DOWNLOAD)" --strip-components=1 -f -)
+	&& for url in $(MAKEFILET_DOWNLOAD_URLS); do \
+		printf >&2 "Trying to download 'makefilet' from $$url ... "; \
+		if wget --quiet --output-document - "$$url"; then \
+			echo >&2 "OK"; \
+			break; \
+		else \
+			echo >&2 "failed"; \
+		fi; \
+	done | tar -xz -C "$(DIR_MAKEFILET_DOWNLOAD)" --strip-components=1 -f -)
 # last include attempt
 -include $(DIR_MAKEFILET_DOWNLOAD)/main.mk
 ifndef DIR_MAKEFILET
