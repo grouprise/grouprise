9ece923171ec52e1e89da941f79851c2b9c6fbf2
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2024-03-22 01:23:23 +0100

    feat: allow tag rendering customization through template context

--- grouprise/features/tags/templatetags/defaulttags.py
+++ grouprise/features/tags/templatetags/defaulttags.py
@@ -5,20 +5,23 @@ from grouprise.features.tags.utils import get_tag_render_data
 register = template.Library()
 
 
-@register.inclusion_tag("tags/_tag.html", name="tag")
-def render_tag(tag, **kwargs):
+@register.inclusion_tag("tags/_tag.html", name="tag", takes_context=True)
+def render_tag(context, tag, **kwargs):
     tag_group, tag_name = get_tag_render_data(tag.name)
-    context = {
-        "tag": {
-            "group": tag_group,
-            "name": tag_name,
-            "slug": tag.slug,
+    context.update(
+        {
+            "tag": {
+                "group": tag_group,
+                "name": tag_name,
+                "slug": tag.slug,
+            }
         }
-    }
+    )
     context.update(kwargs)
     return context
 
 
-@register.inclusion_tag("tags/_tags.html", name="tags")
-def render_tags(tags):
-    return {"tags": tags}
+@register.inclusion_tag("tags/_tags.html", name="tags", takes_context=True)
+def render_tags(context, tags):
+    context.update({"tags": tags})
+    return context
--- grouprise/features/tags/templatetags/tags.py
+++ grouprise/features/tags/templatetags/tags.py
@@ -8,3 +8,9 @@ register = template.Library()
 @register.inclusion_tag("tags/_sidebar_list.html")
 def sidebar_tags():
     return {"tags": get_sorted_featured_tags()}
+
+
+@register.inclusion_tag("tags/_tags.html", name="featured_tags", takes_context=True)
+def render_featured_tags(context):
+    context.update({"tags": get_sorted_featured_tags()})
+    return context
